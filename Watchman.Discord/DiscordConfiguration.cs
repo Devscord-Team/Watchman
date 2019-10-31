5ef45067e9487d2d07940c62eebd1e22f3540605
Marcin Bigos <marcin.bigos99@gmail.com>
2019-10-31 22:21:23 +0100

    update discord configuration

--- Watchman.Discord/DiscordConfiguration.cs
+++ Watchman.Discord/DiscordConfiguration.cs
@@ -7,5 +7,6 @@ namespace Watchman.Discord
     public class DiscordConfiguration
     {
         public string Token { get; set; }
+        public string MongoDbConnectionString { get; set; }
     }
 }
