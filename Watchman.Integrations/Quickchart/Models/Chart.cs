36035e92e4699cf31cb68699dd239db48b266326
Marcin Bigos <marcin.bigos99@gmail.com>
2019-11-01 23:58:24 +0100

    add quickstart integration

---
+++ Watchman.Integrations/Quickchart/Models/Chart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Watchman.Integrations.Quickchart.Models
+{
+    public class Chart
+    {
+        public string Type { get; set; }
+        public IEnumerable<string> Labels { get; set; }
+        public Dataset Data { get; set; }
+    }
+
+    public class Dataset
+    {
+        public string Label { get; set; }
+        public IEnumerable<int> Data { get; set; }
+    }
+}
