ebd753dc342ff835c18b5e767bf82e0206666270
Marcin Bigos <marcin.bigos99@gmail.com>
2019-11-01 23:57:51 +0100

    add stream utils

---
+++ Watchman.Common/Utils/StreamUtils.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Watchman.Common.Extensions;
+
+namespace Watchman.Common.Utils
+{
+    public static class StreamUtils
+    {
+        public static MemoryStream ConvertStreamToMemoryStream(Stream stream)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+
+            if (stream != null)
+            {
+                byte[] buffer = stream.ReadFully();
+
+                if (buffer != null)
+                {
+                    var binaryWriter = new BinaryWriter(memoryStream);
+                    binaryWriter.Write(buffer);
+                }
+            }
+            return memoryStream;
+        }
+    }
+}
