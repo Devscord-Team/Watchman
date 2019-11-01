53904855446e45397efcea79988bd08210ee0458
Marcin Bigos <marcin.bigos99@gmail.com>
2019-11-02 00:22:13 +0100

    improve visual effects

--- Watchman.Discord/Areas/Statistics/Services/ChartsService.cs
+++ Watchman.Discord/Areas/Statistics/Services/ChartsService.cs
@@ -21,14 +21,34 @@ namespace Watchman.Discord.Areas.Statistics.Services
 
         public string GetImageStatisticsPerPeriod(StatisticsReport report)
         {
+            var dates = report.StatisticsPerPeriod.Select(x => x.TimeRange.Start);
+            var period = report.StatisticsPerPeriod.First().Period;
+
+            IEnumerable<string> labels = new List<string>();
+            switch (period)
+            {
+                case Period.Hour:
+                    labels = dates.Select(x => x.ToString("yyyy-MM-dd HH"));
+                    break;
+                case Period.Day:
+                    labels = dates.Select(x => x.ToString("yyyy-MM-dd"));
+                    break;
+                case Period.Week:
+                    labels = dates.Select(x => x.ToString("yyyy-MM-dd"));
+                    break;
+                case Period.Month:
+                    labels = dates.Select(x => x.ToString("yyyy-MM"));
+                    break;
+            }
+
             var chart = new Chart
             {
                 Type = "line",
-                Labels = report.StatisticsPerPeriod.Select(x => x.TimeRange.Start.ToString()),
+                Labels = labels.Select(x => x.ToString()).Reverse(),
                 Data = new Dataset
                 {
                     Label = "Messages",
-                    Data = report.StatisticsPerPeriod.Select(x => x.MessagesQuantity)
+                    Data = report.StatisticsPerPeriod.Select(x => x.MessagesQuantity).Reverse()
                 }
             };
 
