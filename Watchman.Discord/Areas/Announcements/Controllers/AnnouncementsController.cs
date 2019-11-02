bb09b228a70f97c86334c6168ee16f309ebb8d74
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-11-02 18:43:20 +0100

    add autobumping controller and integration with disboard bot

---
+++ Watchman.Discord/Areas/Announcements/Controllers/AnnouncementsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Watchman.Discord.Framework.Architecture.Controllers;
+using Watchman.Integrations.Disboard;
+
+namespace Watchman.Discord.Areas.Announcements.Controllers
+{
+    class AnnouncementsController : ReadAlways, IController
+    {
+        private TimeSpan HowOften => new TimeSpan(0, 0, 15); // every 2 hours and 1 minute
+        private bool _autoBumpIsOn;
+        private ServerBumper _bumper;
+
+        [DiscordCommand("-autobump start")]
+        public void AutoBumpStart(SocketMessage socketMessage)
+        {
+            _autoBumpIsOn = true;
+            socketMessage.Channel.SendMessageAsync("Started auto bumping.");
+
+            _bumper = new ServerBumper(socketMessage.Channel);
+            ManageBumping();
+        }
+
+        [DiscordCommand("-autobump stop")]
+        public void AutoBumpStop(SocketMessage socketMessage)
+        {
+            _autoBumpIsOn = false;
+            socketMessage.Channel.SendMessageAsync("Stopped auto bumping.");
+        }
+
+        private async void ManageBumping()
+        {
+            while (_autoBumpIsOn)
+            {
+                await _bumper.SendBump();
+                await Task.Delay(HowOften);
+            }
+        }
+    }
+}
---
+++ Watchman.Integrations/Disboard/ServerBumper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace Watchman.Integrations.Disboard
+{
+    public class ServerBumper
+    {
+        private const string BumpMessage = "!d bump";
+
+        private readonly ISocketMessageChannel _channel;
+
+        public ServerBumper(ISocketMessageChannel channel)
+        {
+            _channel = channel;
+        }
+
+        public Task SendBump()
+        {
+            this._channel.SendMessageAsync(BumpMessage);
+            return Task.CompletedTask;
+        }
+    }
+}
