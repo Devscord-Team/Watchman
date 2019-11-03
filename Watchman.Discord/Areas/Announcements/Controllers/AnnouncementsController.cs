629ff9167b9efe3f759a58658c6e83457730bde7
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-11-03 15:05:11 +0100

    add ability to bumping many servers at the same time

--- Watchman.Discord/Areas/Announcements/Controllers/AnnouncementsController.cs
+++ Watchman.Discord/Areas/Announcements/Controllers/AnnouncementsController.cs
@@ -9,36 +9,32 @@ namespace Watchman.Discord.Areas.Announcements.Controllers
 {
     class AnnouncementsController : ReadAlways, IController
     {
-        private TimeSpan HowOften => new TimeSpan(2, 1, 0); // every 2 hours and 1 minute
-        private CancellationTokenSource _tokenSource;
-        private ServerBumper _bumper;
+        private readonly ServerBumper _bumper;
+
+        public AnnouncementsController()
+        {
+            _bumper = new ServerBumper();
+        }
 
         [DiscordCommand("-autobump start")]
         public void AutoBumpStart(SocketMessage socketMessage)
         {
-            this._tokenSource = new CancellationTokenSource();
+            var isStarted = _bumper.AddServerChannel(socketMessage.Channel);
 
-            socketMessage.Channel.SendMessageAsync("Started auto bumping.");
-
-            _bumper = new ServerBumper(socketMessage.Channel);
-            _ = BumpUntilNotStopped();
+            if (isStarted.Result)
+            {
+                socketMessage.Channel.SendMessageAsync("Rozpoczęto automatyczne podbijanie.");
+            }
         }
 
         [DiscordCommand("-autobump stop")]
         public void AutoBumpStop(SocketMessage socketMessage)
         {
-            this._tokenSource.Cancel();
-            socketMessage.Channel.SendMessageAsync("Stopped auto bumping.");
-        }
+            var isStopped = _bumper.RemoveServerChannel(socketMessage.Channel);
 
-        private async Task BumpUntilNotStopped()
-        {
-            var factory = new TaskFactory(_tokenSource.Token);
-
-            while (true)
+            if (isStopped.Result)
             {
-                await factory.StartNew(() => _bumper.SendBump());
-                await Task.Delay(HowOften);
+                socketMessage.Channel.SendMessageAsync("Zatrzymano automatyczne podbijanie.");
             }
         }
     }
--- Watchman.Integrations/Disboard/ServerBumper.cs
+++ Watchman.Integrations/Disboard/ServerBumper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Discord.WebSocket;
@@ -8,19 +9,40 @@ namespace Watchman.Integrations.Disboard
 {
     public class ServerBumper
     {
-        private const string BumpMessage = "!d bump";
+        private readonly List<BumpedChannel> _channels;
 
-        private readonly ISocketMessageChannel _channel;
-
-        public ServerBumper(ISocketMessageChannel channel)
+        public ServerBumper()
         {
-            _channel = channel;
+            _channels = new List<BumpedChannel>();
         }
 
-        public Task SendBump()
+        public Task<bool> AddServerChannel(ISocketMessageChannel channel)
         {
-            this._channel.SendMessageAsync(BumpMessage);
-            return Task.CompletedTask;
+            if (_channels.Any(x => x.MessageChannel == channel))
+            {
+                channel.SendMessageAsync("Na tym kanale już trwa podbijanie, nie możesz uruchomić kolejnego procesu podbijania na tym samym kanale.");
+                return Task.FromResult(false);
+            }
+
+            var bumpedChannel = new BumpedChannel(channel);
+            bumpedChannel.BumpServer();
+            this._channels.Add(bumpedChannel);
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> RemoveServerChannel(ISocketMessageChannel channel)
+        {
+            var bumpedChannel = _channels.Find(x => x.MessageChannel == channel);
+
+            if (bumpedChannel == null)
+            {
+                channel.SendMessageAsync("Na tym kanale nie ma żadnego aktywnego podbijania.");
+                return Task.FromResult(false);
+            }
+
+            bumpedChannel.CancelBumping();
+            _channels.Remove(bumpedChannel);
+            return Task.FromResult(true);
         }
     }
 }
