6c4fac45d3707439c9f15b9c89fe1938800b4e1f
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-11-04 16:49:20 +0100

    add AttributeUsage

--- Watchman.Discord/Framework/Architecture/Controllers/ReadAlways.cs
+++ Watchman.Discord/Framework/Architecture/Controllers/ReadAlways.cs
@@ -4,11 +4,11 @@ using System.Text;
 
 namespace Watchman.Discord.Framework.Architecture.Controllers
 {
+    [AttributeUsage(AttributeTargets.Method)]
     public class ReadAlways : Attribute
     {
         public ReadAlways()
         {
-
         }
     }
 }
