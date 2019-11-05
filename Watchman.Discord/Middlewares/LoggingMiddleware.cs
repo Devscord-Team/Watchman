053fc19621748f6dacf04d0adcf9961259a1d80d
Marcin Bigos <marcin.bigos99@gmail.com>
2019-11-05 20:46:50 +0100

    move logging middleware and add empty context to that

---
+++ Watchman.Discord/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,19 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Watchman.Discord.Contexts;
+using Watchman.Discord.Framework.Architecture.Middlewares;
+
+namespace Watchman.Discord.Middlewares
+{
+    public class LoggingMiddleware : IMiddleware<EmptyContext>
+    {
+        public Task<IDiscordContext> Process(SocketMessage data)
+        {
+            ///todo add serilog
+            Console.WriteLine($"{DateTime.Now} {data.Author}: {data.Content}");
+            return default;
+        }
+    }
+}
