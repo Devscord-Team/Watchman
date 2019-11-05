2e659cc6319f227161ce3a8cdb939d94af3ca192
Marcin Bigos <marcin.bigos99@gmail.com>
2019-11-05 20:45:49 +0100

    add empty context

---
+++ Watchman.Discord/Contexts/EmptyContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Watchman.Discord.Framework.Architecture.Middlewares;
+
+namespace Watchman.Discord.Contexts
+{
+    public class EmptyContext : IDiscordContext
+    {
+    }
+}
