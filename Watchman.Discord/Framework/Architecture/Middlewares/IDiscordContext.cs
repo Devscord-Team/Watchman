4d0bde2ed565f348b6ba08c60b24358c1cd6f239
Marcin Bigos <marcin.bigos99@gmail.com>
2019-11-05 20:46:16 +0100

    add imiddleware idiscordcontext

---
+++ Watchman.Discord/Framework/Architecture/Middlewares/IDiscordContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Watchman.Discord.Framework.Architecture.Middlewares
+{
+    public interface IDiscordContext
+    {
+    }
+}
---
+++ Watchman.Discord/Framework/Architecture/Middlewares/IMiddleware.cs
@@ -0,0 +1,13 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Watchman.Discord.Framework.Architecture.Middlewares
+{
+    public interface IMiddleware<T> where T : IDiscordContext
+    {
+        Task<IDiscordContext> Process(SocketMessage data);
+    }
+}
