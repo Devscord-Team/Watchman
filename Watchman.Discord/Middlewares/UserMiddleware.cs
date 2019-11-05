e6221ec803afa7ee0a7a81cd26169808136c8a46
Marcin Bigos <marcin.bigos99@gmail.com>
2019-11-05 20:47:21 +0100

    add empty middlewares

---
+++ Watchman.Discord/Middlewares/UserMiddleware.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Watchman.Discord.Contexts;
+using Watchman.Discord.Framework.Architecture.Middlewares;
+
+namespace Watchman.Discord.Middlewares
+{
+    public class UserMiddleware : IMiddleware<UserContext>
+    {
+        public Task<IDiscordContext> Process(SocketMessage data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
