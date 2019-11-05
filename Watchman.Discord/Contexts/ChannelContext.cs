57df249f41d8e9ca31b9425f9d9812ce95fbd7ed
Marcin Bigos <marcin.bigos99@gmail.com>
2019-11-05 20:45:34 +0100

    add contexts

---
+++ Watchman.Discord/Contexts/ChannelContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Watchman.Discord.Framework.Architecture.Middlewares;
+
+namespace Watchman.Discord.Contexts
+{
+    public class ChannelContext : IDiscordContext
+    {
+        public ulong Id { get; private set; }
+        public string Name { get; private set; }
+    }
+}
---
+++ Watchman.Discord/Contexts/DiscordServerContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Watchman.Discord.Framework.Architecture.Middlewares;
+
+namespace Watchman.Discord.Contexts
+{
+    public class DiscordServerContext : IDiscordContext
+    {
+        public ulong Id { get; private set; }
+        public string Name { get; private set; }
+    }
+}
