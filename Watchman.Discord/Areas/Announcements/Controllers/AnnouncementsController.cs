466a146d12723924d64df31af53515dc8d5ed1d2
Marcin Bigos <marcin.bigos99@gmail.com>
2019-11-22 17:58:35 +0100

    commend announcements controller

--- Watchman.Discord/Areas/Announcements/Controllers/AnnouncementsController.cs
+++ Watchman.Discord/Areas/Announcements/Controllers/AnnouncementsController.cs
@@ -1,55 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
+using Devscord.DiscordFramework.Framework.Architecture.Controllers;
+using Devscord.DiscordFramework.Framework.Architecture.Middlewares;
 using Discord.WebSocket;
-using Watchman.Discord.Framework;
-using Watchman.Discord.Framework.Architecture.Controllers;
-using Watchman.Discord.Framework.Architecture.Middlewares;
-using Watchman.Discord.Middlewares.Contexts;
 using Watchman.Integrations.Disboard;
 
 namespace Watchman.Discord.Areas.Announcements.Controllers
 {
-    class AnnouncementsController : IController
-    {
-        private readonly ServerBumper _bumper;
+    //class AnnouncementsController : IController
+    //{
+    //    private readonly ServerBumper _bumper;
 
-        public AnnouncementsController()
-        {
-            _bumper = new ServerBumper();
-        }
+    //    public AnnouncementsController()
+    //    {
+    //        _bumper = new ServerBumper();
+    //    }
 
-        [AdminCommand]
-        [DiscordCommand("-autobump start")]
-        public void AutoBumpStart(string message, Dictionary<string, IDiscordContext> contexts)
-        {
-            var channel = GetChannel(contexts);
-            var isStarted = _bumper.AddServerChannel(channel);
+    //    [AdminCommand]
+    //    [DiscordCommand("-autobump start")]
+    //    public void AutoBumpStart(string message, Dictionary<string, IDiscordContext> contexts)
+    //    {
+    //        var channel = GetChannel(contexts);
+    //        var isStarted = _bumper.AddServerChannel(channel);
 
-            if (isStarted.Result)
-            {
-                channel.SendMessageAsync("Rozpoczęto automatyczne podbijanie.");
-            }
-        }
+    //        if (isStarted.Result)
+    //        {
+    //            channel.SendMessageAsync("Rozpoczęto automatyczne podbijanie.");
+    //        }
+    //    }
 
-        [AdminCommand]
-        [DiscordCommand("-autobump stop")]
-        public void AutoBumpStop(string message, Dictionary<string, IDiscordContext> contexts)
-        {
-            var channel = GetChannel(contexts);
-            var isStopped = _bumper.RemoveServerChannel(channel);
+    //    [AdminCommand]
+    //    [DiscordCommand("-autobump stop")]
+    //    public void AutoBumpStop(string message, Dictionary<string, IDiscordContext> contexts)
+    //    {
+    //        var channel = GetChannel(contexts);
+    //        var isStopped = _bumper.RemoveServerChannel(channel);
 
-            if (isStopped.Result)
-            {
-                channel.SendMessageAsync("Zatrzymano automatyczne podbijanie.");
-            }
-        }
-
-        private ISocketMessageChannel GetChannel(Dictionary<string, IDiscordContext> contexts)
-        {
-            var channel = ((ChannelContext) contexts[nameof(ChannelContext)]);
-            return (ISocketMessageChannel)Server.GetChannel(channel.Id);
-        }
-    }
+    //        if (isStopped.Result)
+    //        {
+    //            channel.SendMessageAsync("Zatrzymano automatyczne podbijanie.");
+    //        }
+    //    }
+    //}
 }
