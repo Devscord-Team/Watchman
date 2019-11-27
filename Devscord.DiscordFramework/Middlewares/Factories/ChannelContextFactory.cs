923b884c13fee04099ddcef510ff57cd689dcfda
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-11-27 10:02:08 +0100

    add factories

---
+++ Devscord.DiscordFramework/Middlewares/Factories/ChannelContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Devscord.DiscordFramework.Framework.Architecture.Middlewares;
+using Devscord.DiscordFramework.Middlewares.Contexts;
+using Devscord.DiscordFramework.Middlewares.Factories;
+using Discord.WebSocket;
+
+namespace Devscord.DiscordFramework.Middlewares
+{
+    internal class ChannelContextFactory : IContextFactory<ISocketMessageChannel, ChannelContext>
+    {
+        public ChannelContext Create(ISocketMessageChannel socketChannel)
+        {
+            return new ChannelContext(socketChannel.Id, socketChannel.Name);
+        }
+    }
+}
