d6830a275798f7ae1bbe6f0e7606acdef7999e9d
Marcin Bigos <marcin.bigos99@gmail.com>
2019-12-03 19:09:02 +0100

    comment not used code

--- Watchman.Integrations/Disboard/ServerBumper.cs
+++ Watchman.Integrations/Disboard/ServerBumper.cs
@@ -1,48 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Discord.WebSocket;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Discord.WebSocket;
 
-namespace Watchman.Integrations.Disboard
-{
-    public class ServerBumper
-    {
-        private readonly List<BumpedChannel> _channels;
+//namespace Watchman.Integrations.Disboard
+//{
+//    public class ServerBumper
+//    {
+//        private readonly List<BumpedChannel> _channels;
 
-        public ServerBumper()
-        {
-            _channels = new List<BumpedChannel>();
-        }
+//        public ServerBumper()
+//        {
+//            _channels = new List<BumpedChannel>();
+//        }
 
-        public Task<bool> AddServerChannel(ISocketMessageChannel channel)
-        {
-            if (_channels.Any(x => x.MessageChannel == channel))
-            {
-                channel.SendMessageAsync("Na tym kanale już trwa podbijanie, nie możesz uruchomić kolejnego procesu podbijania na tym samym kanale.");
-                return Task.FromResult(false);
-            }
+//        public Task<bool> AddServerChannel(ISocketMessageChannel channel)
+//        {
+//            if (_channels.Any(x => x.MessageChannel == channel))
+//            {
+//                channel.SendMessageAsync("Na tym kanale już trwa podbijanie, nie możesz uruchomić kolejnego procesu podbijania na tym samym kanale.");
+//                return Task.FromResult(false);
+//            }
 
-            var bumpedChannel = new BumpedChannel(channel);
-            bumpedChannel.BumpServer();
-            this._channels.Add(bumpedChannel);
-            return Task.FromResult(true);
-        }
+//            var bumpedChannel = new BumpedChannel(channel);
+//            bumpedChannel.BumpServer();
+//            this._channels.Add(bumpedChannel);
+//            return Task.FromResult(true);
+//        }
 
-        public Task<bool> RemoveServerChannel(ISocketMessageChannel channel)
-        {
-            var bumpedChannel = _channels.Find(x => x.MessageChannel == channel);
+//        public Task<bool> RemoveServerChannel(ISocketMessageChannel channel)
+//        {
+//            var bumpedChannel = _channels.Find(x => x.MessageChannel == channel);
 
-            if (bumpedChannel == null)
-            {
-                channel.SendMessageAsync("Na tym kanale nie ma żadnego aktywnego podbijania.");
-                return Task.FromResult(false);
-            }
+//            if (bumpedChannel == null)
+//            {
+//                channel.SendMessageAsync("Na tym kanale nie ma żadnego aktywnego podbijania.");
+//                return Task.FromResult(false);
+//            }
 
-            bumpedChannel.CancelBumping();
-            _channels.Remove(bumpedChannel);
-            return Task.FromResult(true);
-        }
-    }
-}
+//            bumpedChannel.CancelBumping();
+//            _channels.Remove(bumpedChannel);
+//            return Task.FromResult(true);
+//        }
+//    }
+//}
