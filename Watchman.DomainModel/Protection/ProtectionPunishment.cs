e90c40b1f913b20e57d348586d2e9f6731959c63
Marcin Bigos <marcin.bigos99@gmail.com>
2019-12-03 22:50:24 +0100

    save

---
+++ Watchman.DomainModel/Protection/ProtectionPunishment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Watchman.DomainModel.Protection
+{
+    public class ProtectionPunishment
+    {
+        public ProtectionPunishmentOptions Option { get; private set; }
+        public TimeSpan? Time { get; private set; }
+
+        public ProtectionPunishment(ProtectionPunishmentOptions option, TimeSpan? time = null)
+        {
+            Option = option;
+            Time = time;
+        }
+    }
+}
---
+++ Watchman.DomainModel/Protection/ProtectionPunishmentOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Watchman.DomainModel.Protection
+{
+    public enum ProtectionPunishmentOptions
+    {
+        Nothing,
+        Clear,
+        Alert,
+        Mute,
+        Ban
+    }
+}
