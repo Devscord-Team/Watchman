6b0a0f2ee8ddd8bc75cf264b8a2a1cf2c4e38aea
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-12-06 18:16:46 +0100

    add printing version

---
+++ Watchman.Discord/Areas/BotSettings/Controllers/VersionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Devscord.DiscordFramework.Framework.Architecture.Controllers;
+using Devscord.DiscordFramework.Framework.Architecture.Middlewares;
+using Devscord.DiscordFramework.Middlewares.Contexts;
+using Devscord.DiscordFramework.Services;
+using Watchman.Cqrs;
+
+namespace Watchman.Discord.Areas.BotSettings.Controllers
+{
+    class VersionController
+    {
+        private readonly IQueryBus queryBus;
+        private readonly ICommandBus commandBus;
+
+        public VersionController(IQueryBus queryBus, ICommandBus commandBus)
+        {
+            this.queryBus = queryBus;
+            this.commandBus = commandBus;
+        }
+
+        [DiscordCommand("-version")]
+        public void PrintVersion(string message, Dictionary<string, IDiscordContext> contexts)
+        {
+            var version = File.ReadAllText("version.txt");
+            var channel = (ChannelContext) contexts[nameof(ChannelContext)];
+            var messagesService = new MessagesService() { DefaultChannelId = channel.Id };
+            messagesService.SendMessage($"```Obecna wersja: {version}```");
+        }
+    }
+}
