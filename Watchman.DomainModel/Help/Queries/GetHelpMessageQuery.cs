012a53ac311199bc40e088df9103b61cab5ab40d
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-12-09 12:28:33 +0100

    add query for help

---
+++ Watchman.DomainModel/Help/Queries/GetHelpMessageQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Watchman.Cqrs;
+using Watchman.Integrations.MongoDB;
+
+namespace Watchman.DomainModel.Help.Queries
+{
+    public class GetHelpMessageQuery : IQuery<GetHelpMessageQueryResult>
+    {
+        public ISession Session { get; }
+
+        public GetHelpMessageQuery(ISession session)
+        {
+            this.Session = session;
+        }
+    }
+}
---
+++ Watchman.DomainModel/Help/Queries/Handlers/GetHelpMessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Watchman.Cqrs;
+
+namespace Watchman.DomainModel.Help.Queries.Handlers
+{
+    public class GetHelpMessageQueryHandler : IQueryHandler<GetHelpMessageQuery, GetHelpMessageQueryResult>
+    {
+        public GetHelpMessageQueryResult Handle(GetHelpMessageQuery query)
+        {
+
+            var messageBuilder = new StringBuilder();
+            throw new NotImplementedException();
+            return new GetHelpMessageQueryResult(messageBuilder.ToString());
+        }
+    }
+}
