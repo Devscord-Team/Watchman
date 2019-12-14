ed9cb8efb6ae2aca91561d5f39708283215f7306
Marcin Bigos <marcin.bigos99@gmail.com>
2019-12-14 18:46:58 +0100

    contexts object

---
+++ Devscord.DiscordFramework/Middlewares/Contexts/DiscordContext.cs
@@ -0,0 +1,32 @@
+﻿using Devscord.DiscordFramework.Framework.Architecture.Middlewares;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Devscord.DiscordFramework.Middlewares.Contexts
+{
+    public class Contexts
+    {
+        public DiscordServerContext Server { get; private set; }
+        public ChannelContext Channel { get; private set; }
+        public UserContext User { get; private set; }
+
+        public void SetContext<T>(T context) where T : IDiscordContext
+        {
+            switch (context.GetType().Name)
+            {
+                case "DiscordServerContext":
+                    Server = context as DiscordServerContext;
+                    break;
+
+                case "ChannelContext":
+                    Channel = context as ChannelContext;
+                    break;
+
+                case "UserContext":
+                    User = context as UserContext;
+                    break;
+            }
+        }
+    }
+}
