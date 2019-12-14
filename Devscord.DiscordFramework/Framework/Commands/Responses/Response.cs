95a8180e9f830c4a6c27ee655fdd9d7d0a713dc1
Marcin Bigos <marcin.bigos99@gmail.com>
2019-12-14 16:31:43 +0100

    add response object and service

---
+++ Devscord.DiscordFramework/Framework/Commands/Responses/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Devscord.DiscordFramework.Framework.Commands.Responses
+{
+    public class Response
+    {
+        public string OnEvent { get; set; }
+        public string Message { get; set; }
+    }
+
+}
---
+++ Devscord.DiscordFramework/Framework/Commands/Responses/ResponsesService.cs
@@ -0,0 +1,20 @@
+﻿using Devscord.DiscordFramework.Services;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Devscord.DiscordFramework.Framework.Commands.Responses
+{
+    public class ResponsesService : IService
+    {
+        public IEnumerable<Response> Responses { get; set; }
+        public ResponsesService()
+        {
+            this.Responses = JsonConvert.DeserializeObject<IEnumerable<Response>>(File.ReadAllText("responses-configuration.json"));
+        }
+
+
+    }
+}
