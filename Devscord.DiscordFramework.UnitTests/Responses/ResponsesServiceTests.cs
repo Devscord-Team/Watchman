85771e71ec0bd4ef6b4b222f3d317f6ee4871169
Marcin Bigos <marcin.bigos99@gmail.com>
2019-12-14 16:31:58 +0100

    add tests library

---
+++ Devscord.DiscordFramework.UnitTests/Responses/ResponsesServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Devscord.DiscordFramework.Framework.Commands.Responses;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Devscord.DiscordFramework.UnitTests.Responses
+{
+    [TestFixture]
+    public class ResponsesServiceTests
+    {
+        [Test]
+        public void ShouldFoundManyResponsesInFile()
+        {
+            //Arrange
+            var responsesService = new ResponsesService();
+
+            //Act
+            var responses = responsesService.Responses;
+            var result = responses.Count();
+
+            //Assert
+            Assert.That(result, Is.GreaterThan(0));
+        }
+    }
+}
