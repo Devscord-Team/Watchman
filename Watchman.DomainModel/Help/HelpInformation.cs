4fd0f0d900a3bbd92a7e67cd02e56d3ce763c78f
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-12-16 18:55:05 +0100

    add helpId

--- Watchman.DomainModel/Help/HelpInformation.cs
+++ Watchman.DomainModel/Help/HelpInformation.cs
@@ -13,6 +13,7 @@ namespace Watchman.DomainModel.Help
 
     public class DefaultHelpInformation : Entity
     {
+        public ulong HelpId { get; set; }
         public IEnumerable<string> MethodNames { get; set; }
         public IEnumerable<Description> Descriptions { get; set; }
         public string DefaultDescriptionName { get; set; }
