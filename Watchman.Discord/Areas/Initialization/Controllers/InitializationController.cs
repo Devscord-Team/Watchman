9780d4dbe0d2d36b014a86a4506a4505d74c5b27
Marcin Bigos <marcin.bigos99@gmail.com>
2019-12-21 19:31:21 +0100

    init controller

---
+++ Watchman.Discord/Areas/Initialization/Controllers/InitializationController.cs
@@ -0,0 +1,34 @@
+﻿using Devscord.DiscordFramework.Framework.Architecture.Controllers;
+using Devscord.DiscordFramework.Framework.Commands.Parsing.Models;
+using Devscord.DiscordFramework.Middlewares.Contexts;
+using Devscord.DiscordFramework.Services.Factories;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Watchman.Cqrs;
+
+namespace Watchman.Discord.Areas.Initialization.Controllers
+{
+    public class InitializationController : IController
+    {
+        private readonly IQueryBus queryBus;
+        private readonly ICommandBus commandBus;
+        private readonly MessagesServiceFactory messagesServiceFactory;
+
+        public InitializationController(IQueryBus queryBus, ICommandBus commandBus, MessagesServiceFactory messagesServiceFactory)
+        {
+            this.queryBus = queryBus;
+            this.commandBus = commandBus;
+            this.messagesServiceFactory = messagesServiceFactory;
+        }
+
+        [DiscordCommand("init")]
+        //[IgnoreForHelp] TODO
+        public void Init(DiscordRequest request, Contexts contexts)
+        {
+            var responsesToAdd = File.ReadAllText(@"Framework/Commands/Responses/responses-configuration.json");
+        }
+
+    }
+}
