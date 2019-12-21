19949deabd039671540d96d2f5f2978cb5102510
Marcin Bigos <marcin.bigos99@gmail.com>
2019-12-21 15:44:36 +0100

    parse prefix and name

---
+++ Devscord.DiscordFramework.UnitTests/Parsing/CommandParserTests.cs
@@ -0,0 +1,64 @@
+﻿using Devscord.DiscordFramework.Framework.Commands.Parsing;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Devscord.DiscordFramework.UnitTests.Parsing
+{
+    [TestFixture]
+    public class CommandParserTests
+    {
+        [Test]
+        [TestCase("-help", "-", true)]
+        [TestCase("--help", "--", true)]
+        [TestCase("--help", "-", false)]
+        [TestCase("-help json", "-", true)]
+        [TestCase("!help", "-", false)]
+        [TestCase("!help", "!", true)]
+        [TestCase("help", null, true)]
+        [TestCase("help", "-", false)]
+        public void ShouldFoundPrefix(string message, string prefix, bool shouldTrue)
+        {
+            //Arrange
+            var commandParser = new CommandParser();
+
+            //Act
+            var result = commandParser.Parse(message);
+
+            //Assert
+            if (shouldTrue)
+            {
+                Assert.That(result.Prefix, Is.EqualTo(prefix));
+            }
+            else
+            {
+                Assert.That(result.Prefix, Is.Not.EqualTo(prefix));
+            }
+        }
+
+        [Test]
+        [TestCase("-help", "help", true)]
+        [TestCase("help", "help", true)]
+        [TestCase("!roles", "roles", true)]
+        [TestCase("-good", "bad", false)]
+        public void ShouldFoundName(string message, string name, bool shouldTrue)
+        {
+            //Arrange
+            var commandParser = new CommandParser();
+
+            //Act
+            var result = commandParser.Parse(message);
+
+            //Assert
+            if (shouldTrue)
+            {
+                Assert.That(result.Name, Is.EqualTo(name));
+            }
+            else
+            {
+                Assert.That(result.Name, Is.Not.EqualTo(name));
+            }
+        }
+    }
+}
--- Devscord.DiscordFramework/Framework/Commands/Parsing/CommandParser.cs
+++ Devscord.DiscordFramework/Framework/Commands/Parsing/CommandParser.cs
@@ -1,13 +1,46 @@
-﻿using Devscord.DiscordFramework.Framework.Commands.Parsing.Models;
+﻿using Devscord.DiscordFramework.Commons.Extensions;
+using Devscord.DiscordFramework.Framework.Commands.Parsing.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Devscord.DiscordFramework.Framework.Commands.Parsing
 {
     public class CommandParser
     {
+        private readonly string[] possiblePrefixes = new string[] { "!", "--", "-", "^", "$", "%" };
+
         public DiscordRequest Parse(string message)
+        {
+            var prefix = this.GetPrefix(message);
+            message = message.CutStart(prefix);
+
+            var name = this.GetName(message);
+            message = message.CutStart(name);
+
+            var arguments = this.GetArguments(message);
+
+            return new DiscordRequest
+            {
+                Prefix = prefix,
+                Name = name,
+                ArgumentsPrefix = arguments?.FirstOrDefault()?.Prefix,
+                Arguments = arguments
+            };
+        }
+
+        private string GetPrefix(string message)
+        {
+            return possiblePrefixes.FirstOrDefault(x => message.StartsWith(x));
+        }
+
+        private string GetName(string message)
+        {
+            return message.Split(' ').First();
+        }
+
+        private IEnumerable<DiscordRequestArgument> GetArguments(string message)
         {
             return default;
         }
