750b946f5d965b090562899b1caa43961b405b0b
Marcin Bigos <marcin.bigos99@gmail.com>
2019-12-21 20:42:06 +0100

    fix

--- Watchman.Discord/Areas/Users/Controllers/UsersController.cs
+++ Watchman.Discord/Areas/Users/Controllers/UsersController.cs
@@ -45,7 +45,7 @@ namespace Watchman.Discord.Areas.Users.Controllers
         [DiscordCommand("add role")] //todo
         public void AddRole(DiscordRequest request, Contexts contexts)
         {
-            var commandRole = request.OriginalMessage.ToLowerInvariant().Replace("-add-role ", string.Empty); //TODO use DiscordRequest properties
+            var commandRole = request.OriginalMessage.ToLowerInvariant().Replace("-add role ", string.Empty); //TODO use DiscordRequest properties
             var safeRoles = this.queryBus.Execute(new GetDiscordServerSafeRolesQuery()).SafeRoles;
             var messagesService = messagesServiceFactory.Create(contexts);
             rolesService.AddRoleToUser(safeRoles, messagesService, contexts, commandRole);
@@ -54,7 +54,7 @@ namespace Watchman.Discord.Areas.Users.Controllers
         [DiscordCommand("remove role")] //todo
         public void RemoveRole(DiscordRequest request, Contexts contexts)
         {
-            var commandRole = request.OriginalMessage.ToLowerInvariant().Replace("-remove-role ", string.Empty); //TODO use DiscordRequest properties
+            var commandRole = request.OriginalMessage.ToLowerInvariant().Replace("-remove role ", string.Empty); //TODO use DiscordRequest properties
             var safeRoles = this.queryBus.Execute(new GetDiscordServerSafeRolesQuery()).SafeRoles;
             var messagesService = messagesServiceFactory.Create(contexts);
             rolesService.DeleteRoleFromUser(safeRoles, messagesService, contexts, commandRole);
