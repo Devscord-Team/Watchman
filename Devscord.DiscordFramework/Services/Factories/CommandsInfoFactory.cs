4a307c8f708796075da8816ebaa2cb12a95843bc
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-12-21 22:51:21 +0100

    add factory

--- Devscord.DiscordFramework/Services/Factories/CommandsInfoFactory.cs
+++ Devscord.DiscordFramework/Services/Factories/CommandsInfoFactory.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Devscord.DiscordFramework.Commons.Extensions;
+using Devscord.DiscordFramework.Framework.Architecture.Controllers;
 using Devscord.DiscordFramework.Services.Models;
 
 namespace Devscord.DiscordFramework.Services.Factories
@@ -9,7 +12,19 @@ namespace Devscord.DiscordFramework.Services.Factories
     {
         public IEnumerable<CommandInfo> Create(Type controller)
         {
-            controller.GetMethod().
+            var methods = controller.GetMethods().GetMethodsByAttribute<DiscordCommand>();
+            return methods.Select(x => new CommandInfo
+            {
+                // todo: sprawdzić jeszcze raz
+                Prefix = x.CustomAttributes.GetAttributes<DiscordCommand>().First().ConstructorArguments.First().ToString()[0].ToString(),
+                Names = x.CustomAttributes.GetAttributes<DiscordCommand>().Select(x => x.ConstructorArguments.First().ToString()),
+                MethodName = x.Name,
+                CommandArgumentInfos = x.CustomAttributes.Select(x => new CommandArgumentInfo
+                {
+                    ArgumentPrefix = "",
+                    Name = "Default"
+                })
+            });
         }
     }
 }
