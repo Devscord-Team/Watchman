41b90466665c1571d8d5ef797e6f25a175b3f7cf
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-12-22 15:38:46 +0100

    use jsonConvert

--- Watchman.Discord/Areas/Help/Controllers/HelpController.cs
+++ Watchman.Discord/Areas/Help/Controllers/HelpController.cs
@@ -1,13 +1,11 @@
-﻿using Devscord.DiscordFramework.Framework.Architecture.Controllers;
-using Devscord.DiscordFramework.Framework.Architecture.Middlewares;
+﻿using System.Collections.Generic;
+using Devscord.DiscordFramework.Framework.Architecture.Controllers;
 using Devscord.DiscordFramework.Framework.Commands.Parsing.Models;
 using Devscord.DiscordFramework.Middlewares.Contexts;
-using Devscord.DiscordFramework.Services;
 using Devscord.DiscordFramework.Services.Factories;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Newtonsoft.Json;
 using Watchman.Cqrs;
 using Watchman.DomainModel.Help.Queries;
 using Watchman.Integrations.MongoDB;
@@ -17,16 +15,12 @@ namespace Watchman.Discord.Areas.Help.Controllers
     public class HelpController : IController
     {
         private readonly IQueryBus _queryBus;
-        private readonly ICommandBus _commandBus;
         private readonly MessagesServiceFactory _messagesServiceFactory;
-        private readonly ISession _session;
 
-        public HelpController(IQueryBus queryBus, ICommandBus commandBus, ISessionFactory sessionFactory, MessagesServiceFactory messagesServiceFactory)
+        public HelpController(IQueryBus queryBus, MessagesServiceFactory messagesServiceFactory)
         {
             this._queryBus = queryBus;
-            this._commandBus = commandBus;
             this._messagesServiceFactory = messagesServiceFactory;
-            this._session = sessionFactory.Create();
         }
 
         [DiscordCommand("help")]
@@ -64,35 +58,15 @@ namespace Watchman.Discord.Areas.Help.Controllers
         {
             var result = this._queryBus.Execute(new GetHelpInformationQuery(contexts.Server.Id));
 
+            var serialized = JsonConvert.SerializeObject(result.HelpInformations, Formatting.Indented);
             var messageBuilder = new StringBuilder();
             messageBuilder.AppendLine("```json");
-
-            foreach (var helpInfo in result.HelpInformations)
-            {
-                messageBuilder.AppendLine("{");
-
-                messageBuilder.AppendLine($"\t\"commandId\" : \"{helpInfo.Id}\",");
-                messageBuilder.AppendLine($"\t\"methodName\" : \"{helpInfo.MethodName}\",");
-                messageBuilder.AppendLine($"\t\"descriptions\" : [");
-
-                foreach (var description in helpInfo.Descriptions)
-                {
-                    messageBuilder.AppendLine("\t\t{");
-                    messageBuilder.AppendLine($"\t\t\t\"name\" : \"{description.Name}\",");
-                    messageBuilder.AppendLine($"\t\t\t\"description\" : \"{description.Details}\"");
-                    messageBuilder.AppendLine("\t\t},");
-                }
-                messageBuilder.Remove(messageBuilder.Length - 1, 1);
-
-                messageBuilder.AppendLine("\t]");
-                messageBuilder.AppendLine("},");
-            }
-
-            messageBuilder.Remove(messageBuilder.Length - 1, 1);
+            messageBuilder.AppendLine(serialized);
             messageBuilder.AppendLine("```");
 
             var messagesService = _messagesServiceFactory.Create(contexts);
-            messagesService.SendMessage(messageBuilder.ToString());
+            var message = messageBuilder.ToString();
+            messagesService.SendMessage(message);
         }
     }
 }
