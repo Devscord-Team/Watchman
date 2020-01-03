46ef07ecc53401051a8d5ef25c5a5761414f150b
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-01-03 22:45:20 +0100

    use one instance of commandsInfoFactory

--- Devscord.DiscordFramework/Services/HelpDataCollector.cs
+++ Devscord.DiscordFramework/Services/HelpDataCollector.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
-using Autofac;
 using Devscord.DiscordFramework.Commons.Extensions;
 using Devscord.DiscordFramework.Framework.Architecture.Controllers;
 using Devscord.DiscordFramework.Services.Factories;
@@ -13,18 +12,17 @@ namespace Devscord.DiscordFramework.Services
 {
     public class HelpDataCollector : IService
     {
-        private readonly IComponentContext _componentContext;
+        private readonly CommandsInfoFactory _commandsInfoFactory;
 
-        public HelpDataCollector(IComponentContext componentContext)
+        public HelpDataCollector()
         {
-            this._componentContext = componentContext;
+            this._commandsInfoFactory = new CommandsInfoFactory();
         }
 
         public IEnumerable<CommandInfo> GetCommandsInfo(Assembly botAssembly)
         {
             var controllers = botAssembly.GetTypesByInterface<IController>();
-            var commandFactory = new CommandsInfoFactory();
-            return controllers.SelectMany(x => commandFactory.Create(x));
+            return controllers.SelectMany(x => _commandsInfoFactory.Create(x));
         }
     }
 }
