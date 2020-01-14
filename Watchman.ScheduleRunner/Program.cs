06907b5aa807fb471b1bdc0e561ff4272ab8b498
Marcin Bigos <marcin.bigos99@gmail.com>
2020-01-14 18:17:22 +0100

    add project

---
+++ Watchman.ScheduleRunner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Watchman.ScheduleRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Watchman.ScheduleRunner/Runner.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Watchman.ScheduleRunner
+{
+    public class Runner
+    {
+
+    }
+}
