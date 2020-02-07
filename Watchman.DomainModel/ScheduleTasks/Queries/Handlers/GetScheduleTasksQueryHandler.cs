1c9ffa2512d45dbea553aeb1ff32e2095ea19d86
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-02-07 20:08:28 +0100

    remove not used code

--- Watchman.DomainModel/ScheduleTasks/Queries/Handlers/GetScheduleTasksQueryHandler.cs
+++ Watchman.DomainModel/ScheduleTasks/Queries/Handlers/GetScheduleTasksQueryHandler.cs
@@ -6,24 +6,22 @@ namespace Watchman.DomainModel.ScheduleTasks.Queries.Handlers
 {
     public class GetScheduleTasksQueryHandler : IQueryHandler<GetScheduleTasksQuery, GetScheduleTasksQueryResult>
     {
-        private readonly ISessionFactory sessionFactory;
+        private readonly ISessionFactory _sessionFactory;
 
         public GetScheduleTasksQueryHandler(ISessionFactory sessionFactory)
         {
-            this.sessionFactory = sessionFactory;
+            this._sessionFactory = sessionFactory;
         }
 
         public GetScheduleTasksQueryResult Handle(GetScheduleTasksQuery query)
         {
-            using (var session = sessionFactory.Create())
+            using var session = _sessionFactory.Create();
+            var results = session.Get<ScheduleTask>();
+            if(query.LoadOnlyActive)
             {
-                var results = session.Get<ScheduleTask>();
-                if(query.LoadOnlyActive)
-                {
-                    results = results.Where(x => !x.IsExecuted);
-                }
-                return new GetScheduleTasksQueryResult(results.ToList());
+                results = results.Where(x => !x.IsExecuted);
             }
+            return new GetScheduleTasksQueryResult(results.ToList());
         }
     }
 }
