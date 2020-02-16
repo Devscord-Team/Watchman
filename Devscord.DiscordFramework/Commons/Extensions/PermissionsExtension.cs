99602037ce2024ae34143a651b6270eba8d3c872
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-02-16 15:34:43 +0100

    Update Devscord.DiscordFramework/Commons/Extensions/PermissionsExtension.cs
    Co-Authored-By: Marcin Bigos <marcin.bigos99@gmail.com>

--- Devscord.DiscordFramework/Commons/Extensions/PermissionsExtension.cs
+++ Devscord.DiscordFramework/Commons/Extensions/PermissionsExtension.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 namespace Devscord.DiscordFramework.Commons.Extensions
 {
-    internal static class PermissionsExtension
+    internal static class PermissionsExtensions
     {
         internal static ulong GetRawValue(this ICollection<Permission> permissions)
         {
