db2f3751a7eb1e4eb53999dba1466c7e9a5e11b4
Marcin Bigos <marcin.bigos99@gmail.com>
2020-02-19 20:53:32 +0100

    helps controller

---
+++ Watchman.Web.Server/Areas/Helps/Controllers/HelpsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Watchman.Cqrs;
+using Watchman.DomainModel.Help;
+using Watchman.DomainModel.Help.Queries;
+
+namespace Watchman.Web.Server.Areas.Helps.Controllers
+{
+    public class HelpsController
+    {
+        private readonly IQueryBus queryBus;
+
+        public HelpsController(IQueryBus queryBus)
+        {
+            this.queryBus = queryBus;
+        }
+
+        [HttpGet]
+        public IEnumerable<HelpInformation> GetHelpInformations(ulong serverId = )
+        {
+            var query = new GetHelpInformationQuery(serverId);
+            var responses = queryBus.Execute(query).HelpInformations;
+            return responses;
+        }
+    }
+}
