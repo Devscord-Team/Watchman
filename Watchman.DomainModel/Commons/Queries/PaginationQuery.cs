8892d3ff4999acc9bb755e2ed419c2837f151ab4
Marcin Bigos <marcin.bigos99@gmail.com>
2020-02-23 19:14:34 +0100

    pagination querry

---
+++ Watchman.DomainModel/Commons/Queries/PaginationQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Watchman.DomainModel.Commons.Queries
+{
+    public abstract class PaginationQuery
+    {
+        public int? StartFrom { get; set; }
+        public int? Quantity { get; set; }
+    }
+
+    public abstract class PaginationQuery<T> : PaginationQuery
+    {
+        public Func<T, bool> Filter { get; set; }
+    }
+}
