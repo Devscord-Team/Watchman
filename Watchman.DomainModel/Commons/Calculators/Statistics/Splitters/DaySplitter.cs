f3df9f4c102393c70df0ccaf1c7f34852de4728c
Marcin Bigos <marcin.bigos99@gmail.com>
2020-02-23 23:39:50 +0100

    day splitter

--- Watchman.DomainModel/Commons/Calculators/Statistics/Splitters/DaySplitter.cs
+++ Watchman.DomainModel/Commons/Calculators/Statistics/Splitters/DaySplitter.cs
@@ -11,11 +11,11 @@ namespace Watchman.DomainModel.Commons.Calculators.Statistics.Splitters
     {
         public IEnumerable<KeyValuePair<TimeRange, IEnumerable<T>>> Split<T>(IEnumerable<T> collection) where T : Entity
         {
-            var fullRange = TimeRange.Create(collection.First().CreatedAt.Date, collection.Last().CreatedAt.Date);
-            var groupedByDay = collection.GroupBy(x => x.CreatedAt.Date)
-                .Select(x => );
+            var groupedByDay = collection.GroupBy(x => x.CreatedAt.Date);
             foreach (var day in groupedByDay)
             {
+                var timeRange = TimeRange.Create(day.Key, day.Key);
+                yield return new KeyValuePair<TimeRange, IEnumerable<T>>(timeRange, day);
             }
         }
     }
