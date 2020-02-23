81d6fbd600763550d82b43bbe47600e08c9232b9
Marcin Bigos <marcin.bigos99@gmail.com>
2020-02-24 00:12:25 +0100

    query handler for statistics

---
+++ Watchman.DomainModel/Messages/Queries/GetMessagesStatisticsQueryResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Watchman.Cqrs;
+using Watchman.DomainModel.Messages.Services;
+
+namespace Watchman.DomainModel.Messages.Queries
+{
+    public class GetMessagesStatisticsQueryResult : IQueryResult
+    {
+        public IEnumerable<PeriodStatistic> PeriodStatistics { get; private set; }
+
+        public GetMessagesStatisticsQueryResult(IEnumerable<PeriodStatistic> periodStatistics)
+        {
+            PeriodStatistics = periodStatistics;
+        }
+    }
+}
---
+++ Watchman.DomainModel/Messages/Queries/Handlers/GetMessagesStatisticsQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Watchman.Cqrs;
+using Watchman.DomainModel.Commons.Calculators.Statistics;
+using Watchman.DomainModel.Commons.Queries.Handlers;
+using Watchman.Integrations.MongoDB;
+
+namespace Watchman.DomainModel.Messages.Queries.Handlers
+{
+    public class GetMessagesStatisticsQueryHandler : PaginationQueryHandler, IQueryHandler<GetMessagesStatisticsQuery, GetMessagesStatisticsQueryResult>
+    {
+        private readonly ISessionFactory sessionFactory;
+        private readonly IStatisticsCalculator statisticsCalculator;
+
+        public GetMessagesStatisticsQueryHandler(ISessionFactory sessionFactory, IStatisticsCalculator statisticsCalculator)
+        {
+            this.sessionFactory = sessionFactory;
+            this.statisticsCalculator = statisticsCalculator;
+        }
+
+        public GetMessagesStatisticsQueryResult Handle(GetMessagesStatisticsQuery query)
+        {
+            using (var session = this.sessionFactory.Create())
+            {
+                var messages = session.Get<Message>();
+                var paginated = this.Paginate(query, messages);
+                var statistics = this.statisticsCalculator.GetStatisticsPerPeriod(paginated, query.Period);
+                return new GetMessagesStatisticsQueryResult(statistics);
+            }
+        }
+    }
+}
