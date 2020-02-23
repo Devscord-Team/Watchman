85c662f509697f6c9aa3c14f86be1294f9c19c1c
Marcin Bigos <marcin.bigos99@gmail.com>
2020-02-23 19:15:24 +0100

    add message command handler

---
+++ Watchman.DomainModel/Messages/Commands/Handlers/AddMessageCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Watchman.Cqrs;
+using Watchman.Integrations.MongoDB;
+
+namespace Watchman.DomainModel.Messages.Commands.Handlers
+{
+    public class AddMessageCommandHandler : ICommandHandler<AddMessageCommand>
+    {
+        private readonly ISessionFactory sessionFactory;
+
+        public AddMessageCommandHandler(ISessionFactory sessionFactory)
+        {
+            this.sessionFactory = sessionFactory;
+        }
+
+        public Task HandleAsync(AddMessageCommand command)
+        {
+            var message = Message.Create(command.Content)
+                .WithAuthor(command.AuthorId, command.AuthorName)
+                .WithChannel(command.ChannelId, command.ChannelName)
+                .WithServer(command.ServerId, command.ServerName, command.ServerOwnerId, command.ServerOwnerName)
+                .Build();
+
+            using(var session = this.sessionFactory.Create())
+            {
+                session.Add(message);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
