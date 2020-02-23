f51f179eca537d4b363659cdbf18da761cf4643f
Marcin Bigos <marcin.bigos99@gmail.com>
2020-02-23 19:15:47 +0100

    get messages query

---
+++ Watchman.DomainModel/Messages/Queries/GetMessagesQueryResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Watchman.Cqrs;
+
+namespace Watchman.DomainModel.Messages.Queries
+{
+    public class GetMessagesQueryResult : IQueryResult
+    {
+        public IEnumerable<Message> Messages { get; private set; }
+
+        public GetMessagesQueryResult(IEnumerable<Message> messages)
+        {
+            Messages = messages;
+        }
+    }
+}
