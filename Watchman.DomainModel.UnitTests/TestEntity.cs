2aa2e7b12ba9548b2c84d30354b3b9916b54a86b
Marcin Bigos <marcin.bigos99@gmail.com>
2020-02-23 21:49:18 +0100

    test entity generator

---
+++ Watchman.DomainModel.UnitTests/TestEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Watchman.Integrations.MongoDB;
+
+namespace Watchman.DomainModel.UnitTests
+{
+    public class TestEntity : Entity
+    {
+        public void SetUpdatedAt(DateTime dateTime)
+        {
+            UpdatedAt = dateTime;
+        }
+
+        public void SetCreatedAt(DateTime dateTime)
+        {
+            CreatedAt = dateTime;
+        }
+    }
+}
---
+++ Watchman.DomainModel.UnitTests/TestEntityGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Watchman.Common.Models;
+
+namespace Watchman.DomainModel.UnitTests
+{
+    public static class TestEntityGenerator
+    {
+        public static IEnumerable<TestEntity> Generate(TimeRange timeRange, int quantity)
+        {
+            var ticksBetweenTimes = timeRange.End.Ticks - timeRange.Start.Ticks;
+            var ticksPerItem = (long)Math.Truncate((decimal)ticksBetweenTimes / quantity);
+            foreach (var index in Enumerable.Range(0, quantity))
+            {
+                var item = new TestEntity();
+                var time = timeRange.Start.AddTicks(ticksPerItem * index);
+                item.SetCreatedAt(time);
+                item.SetUpdatedAt(time);
+                yield return item;
+            }
+        }
+    }
+}
