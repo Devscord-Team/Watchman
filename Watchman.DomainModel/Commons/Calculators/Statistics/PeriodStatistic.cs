fef1a30b5cb35aafb58b7c7217ddfc71f0209883
Marcin Bigos <marcin.bigos99@gmail.com>
2020-02-23 20:56:31 +0100

    statistics calculator

---
+++ Watchman.DomainModel/Commons/Calculators/Statistics/PeriodStatistic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Watchman.Common.Models;
+
+namespace Watchman.DomainModel.Messages.Services
+{
+    public class PeriodStatistic
+    {
+        public TimeRange TimeRange { get; private set; }
+        public int Count { get; private set; }
+
+        public PeriodStatistic(TimeRange timeRange, int count)
+        {
+            TimeRange = timeRange;
+            Count = count;
+        }
+    }
+}
---
+++ Watchman.DomainModel/Commons/Calculators/Statistics/StatisticsCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Watchman.Common.Models;
+using Watchman.DomainModel.Commons.Calculators.Statistics.Splitters;
+using Watchman.DomainModel.Messages.Services;
+using Watchman.Integrations.MongoDB;
+
+namespace Watchman.DomainModel.Commons.Calculators.Statistics
+{
+    public class StatisticsCalculator
+    {
+        public IEnumerable<PeriodStatistic> GetStatisticsPerPeriod<T>(IEnumerable<T> collection, Period period) where T : Entity
+        {
+            var splitter = this.GetSplitter(period);
+            return this.GetStatisticsPerPeriod(collection, splitter);
+        }
+
+        public IEnumerable<PeriodStatistic> GetStatisticsPerPeriod<T>(IEnumerable<T> collection, ISplitter splitter) where T : Entity
+        {
+            var splitted = splitter.Split(collection);
+            foreach (var singlePeriod in splitted)
+            {
+                yield return new PeriodStatistic(singlePeriod.Key, singlePeriod.Count());
+            }
+        }
+
+        private ISplitter GetSplitter(Period period)
+        {
+            return period switch
+            {
+                Period.Hour => new HourSplitter(),
+                Period.Day => new DaySplitter(),
+                Period.Week => new WeekSplitter(),
+                Period.Month => new WeekSplitter(),
+                _ => default
+            };
+        }
+    }
+}
