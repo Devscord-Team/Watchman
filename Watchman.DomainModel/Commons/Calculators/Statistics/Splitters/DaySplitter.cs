380ec97d4ea05982615c560cf6298ba52dc7ced2
Marcin Bigos <marcin.bigos99@gmail.com>
2020-02-23 20:56:12 +0100

    add splitters

---
+++ Watchman.DomainModel/Commons/Calculators/Statistics/Splitters/DaySplitter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Watchman.Common.Models;
+using Watchman.Integrations.MongoDB;
+
+namespace Watchman.DomainModel.Commons.Calculators.Statistics.Splitters
+{
+    public class DaySplitter : ISplitter
+    {
+        public IEnumerable<IGrouping<TimeRange, T>> Split<T>(IEnumerable<T> collection) where T : Entity
+        {
+            var latestDate = collection.First().CreatedAt;
+            var oldestDate = collection.Last().CreatedAt;
+            return default;
+        }
+    }
+}
---
+++ Watchman.DomainModel/Commons/Calculators/Statistics/Splitters/WeekSplitter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Watchman.Common.Models;
+using Watchman.Integrations.MongoDB;
+
+namespace Watchman.DomainModel.Commons.Calculators.Statistics.Splitters
+{
+    public class WeekSplitter : ISplitter
+    {
+        public IEnumerable<IGrouping<TimeRange, T>> Split<T>(IEnumerable<T> collection) where T : Entity
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
