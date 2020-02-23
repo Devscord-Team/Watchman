0c3525c4553a163465b33ae0406a5ab97dce039a
Marcin Bigos <marcin.bigos99@gmail.com>
2020-02-23 21:49:03 +0100

    testable entity

--- Watchman.Integrations/MongoDB/Entity.cs
+++ Watchman.Integrations/MongoDB/Entity.cs
@@ -6,10 +6,10 @@ namespace Watchman.Integrations.MongoDB
 {
     public abstract class Entity
     {
-        public Guid Id { get; private set; } = Guid.NewGuid();
-        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
-        public DateTime UpdatedAt { get; private set; } = DateTime.UtcNow;
-        public int Version { get; private set; }
+        public Guid Id { get; protected set; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; protected set; } = DateTime.UtcNow;
+        public int Version { get; protected set; }
 
         protected void Update()
         {
