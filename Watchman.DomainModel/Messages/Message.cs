0e4cfa520dd1b53d0dff9c0ae88992a7aac2013d
Marcin Bigos <marcin.bigos99@gmail.com>
2020-02-23 18:33:57 +0100

    message

---
+++ Watchman.DomainModel/Messages/Message.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Watchman.Integrations.MongoDB;
+
+namespace Watchman.DomainModel.Messages
+{
+    public class Message : Entity, IAggregateRoot
+    {
+        public User Author { get; private set; }
+        public Channel Channel { get; private set; }
+        public Server Server { get; private set; }
+        public string Content { get; private set; }
+
+        private Message()
+        {
+        }
+
+        public static MessageBuilder Create()
+        {
+            return new MessageBuilder(new Message());
+        }
+
+        public void SetAuthor(User author)
+        {
+            if(author == this.Author)
+            {
+                return;
+            }
+            this.Author = author;
+            this.Update();
+        }
+
+        public void SetChannel(Channel channel)
+        {
+            if (channel == this.Channel)
+            {
+                return;
+            }
+            this.Channel = channel;
+            this.Update();
+        }
+
+        public void SetServer(Server server)
+        {
+            if (server == this.Server)
+            {
+                return;
+            }
+            this.Server = server;
+            this.Update();
+        }
+
+        public void SetContent(string content)
+        {
+            if (content == this.Content)
+            {
+                return;
+            }
+            this.Content = content;
+            this.Update();
+        }
+    }
+
+}
