69b54625bdec33dc09c6e61305c7cbdacfe404a7
Marcin Bigos <marcin.bigos99@gmail.com>
2020-02-23 18:33:26 +0100

    user

---
+++ Watchman.DomainModel/Messages/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Watchman.DomainModel.Messages
+{
+    public class User
+    {
+        public ulong Id { get; private set; }
+        public string Name { get; private set; }
+
+        public User(ulong id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+
+}
