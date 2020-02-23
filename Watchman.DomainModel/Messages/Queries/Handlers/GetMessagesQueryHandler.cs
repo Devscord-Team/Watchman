2e3161f26895760bbe5cbeec46bf9bf129590be9
Marcin Bigos <marcin.bigos99@gmail.com>
2020-02-23 19:38:03 +0100

    add query handler for messages

---
+++ Watchman.DomainModel/Messages/Queries/Handlers/GetMessagesQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Watchman.Cqrs;
+using Watchman.DomainModel.Commons.Queries;
+using Watchman.DomainModel.Commons.Queries.Handlers;
+using Watchman.Integrations.MongoDB;
+
+namespace Watchman.DomainModel.Messages.Queries.Handlers
+{
+    public class GetMessagesQueryHandler : PaginationQueryHandler, IQueryHandler<GetMessagesQuery, GetMessagesQueryResult>
+    {
+        private readonly ISessionFactory sessionFactory;
+
+        public GetMessagesQueryHandler(ISessionFactory sessionFactory)
+        {
+            this.sessionFactory = sessionFactory;
+        }
+
+        public GetMessagesQueryResult Handle(GetMessagesQuery query)
+        {
+            using (var session = sessionFactory.Create())
+            {
+                var messages = session.Get<Message>();
+                var paginated = this.Paginate(query, messages);
+                return new GetMessagesQueryResult(paginated);
+            }
+        }
+    }
+}
