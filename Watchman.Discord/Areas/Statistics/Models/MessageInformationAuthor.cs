328f6de6729e65a1ec31bb4cf4260350e7654d07
Marcin Bigos <marcin.bigos99@gmail.com>
2020-02-23 20:04:28 +0100

    delete old information entities

--- Watchman.Discord/Areas/Statistics/Models/MessageInformationAuthor.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Watchman.Discord.Areas.Statistics.Models
-{
-    public class MessageInformationAuthor
-    {
-        public ulong Id { get; set; }
-        public string Name { get; set; }
-    }
-}
