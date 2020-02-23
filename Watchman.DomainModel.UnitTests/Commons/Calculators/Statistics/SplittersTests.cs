4920f6915d37c7a50e41d23b2038196c6442c35d
Marcin Bigos <marcin.bigos99@gmail.com>
2020-02-23 20:56:41 +0100

    empty tests

---
+++ Watchman.DomainModel.UnitTests/Commons/Calculators/Statistics/SplittersTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Watchman.DomainModel.UnitTests.Commons.Calculators.Statistics
+{
+
+    public class SplittersTests
+    {
+    }
+}
