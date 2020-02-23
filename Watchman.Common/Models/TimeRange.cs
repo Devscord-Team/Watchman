d942dd60a4b82292741515dbefd0aee2b508c7d4
Marcin Bigos <marcin.bigos99@gmail.com>
2020-02-23 21:48:50 +0100

    better timerange

--- Watchman.Common/Models/TimeRange.cs
+++ Watchman.Common/Models/TimeRange.cs
@@ -6,5 +6,17 @@ namespace Watchman.Common.Models
     {
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
+
+        public TimeRange()
+        {
+        }
+
+        public TimeRange(DateTime start, DateTime end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public static TimeRange Create(DateTime start, DateTime end) => new TimeRange(start, end);
     }
 }
