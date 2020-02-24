88c2b022517bda0fddee2e883cc57c946950b72b
Marcin Bigos <marcin.bigos99@gmail.com>
2020-02-24 21:36:44 +0100

    Revert "trying to fix chartjs"
    This reverts commit 2dc08047acf5d1bf69951d1a2b899fa8efd98731.

--- Watchman.Web.Client/watchman/src/app/statistics/statistics.component.ts
+++ Watchman.Web.Client/watchman/src/app/statistics/statistics.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StatisticsService, PeriodStatisticDto } from './services/statistics.service';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { Label } from 'ng2-charts';
-import * as chartDataLabels from 'chartjs-plugin-datalabels';
+import * as pluginDataLabels from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'app-statistics',
@@ -28,7 +28,7 @@ export class StatisticsComponent implements OnInit {
   public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
-  public barChartPlugins = [chartDataLabels];
+  public barChartPlugins = [pluginDataLabels];
 
   public barChartData: ChartDataSets[] = [
     { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
