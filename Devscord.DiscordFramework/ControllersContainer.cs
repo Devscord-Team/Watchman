26bcd03489e3bb8132e54746cb00b555d597dfd6
Marcin Bigos <marcin.bigos99@gmail.com>
2020-02-25 00:40:25 +0100

    controllers container

---
+++ Devscord.DiscordFramework/ControllersContainer.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using Devscord.DiscordFramework.Commons.Extensions;
+using Devscord.DiscordFramework.Framework.Architecture.Controllers;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Devscord.DiscordFramework
+{
+    internal class ControllersContainer
+    {
+        private IEnumerable<ControllerInfo> _controllers;
+
+        public ControllersContainer(IEnumerable<ControllerInfo> controllers)
+        {
+            this._controllers = controllers;
+        }
+
+        public IEnumerable<ControllerInfo> WithReadAlways
+            => this._controllers.Select(x => new ControllerInfo(x.Controller, x.Methods.Where(m => m.HasAttribute<ReadAlways>())));
+
+        public IEnumerable<ControllerInfo> WithDiscordCommand
+            => this._controllers.Select(x => new ControllerInfo(x.Controller, x.Methods.Where(m => m.HasAttribute<DiscordCommand>())));
+    }
+}
