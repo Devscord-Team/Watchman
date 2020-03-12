2a3c63f65f46dd91b5f6dc1c009d30b23910f9bf
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-03-12 17:06:30 +0100

    add command

---
+++ Watchman.DomainModel/Messages/Commands/AddMessagesCommand.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Watchman.Cqrs;
+
+namespace Watchman.DomainModel.Messages.Commands
+{
+    public class AddMessagesCommand : ICommand
+    {
+        public IEnumerable<Message> Messages { get; private set; }
+
+        public AddMessagesCommand(IEnumerable<Message> messages)
+        {
+            Messages = messages;
+        }
+    }
+}
---
+++ Watchman.DomainModel/Messages/Commands/Handlers/AddMessagesCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Watchman.Cqrs;
+using Watchman.Integrations.MongoDB;
+
+namespace Watchman.DomainModel.Messages.Commands.Handlers
+{
+    public class AddMessagesCommandHandler : ICommandHandler<AddMessagesCommand>
+    {
+        private readonly ISessionFactory _sessionFactory;
+
+        public AddMessagesCommandHandler(ISessionFactory sessionFactory)
+        {
+            _sessionFactory = sessionFactory;
+        }
+
+        public async Task HandleAsync(AddMessagesCommand command)
+        {
+            var newMessages = await Task.Run(() => GetOnlyNewMessages(command.Messages));
+            using var session = _sessionFactory.Create();
+            session.Add(newMessages);
+        }
+
+        private IEnumerable<Message> GetOnlyNewMessages(IEnumerable<Message> messages)
+        {
+            using var session = _sessionFactory.Create();
+            var existingMessagesIds = session.Get<Message>()
+                .Select(x => x.Id.GetHashCode())
+                .OrderBy(x => x)
+                .ToList();
+
+            return messages.Where(x =>
+            {
+                var isMessageNew = existingMessagesIds.BinarySearch(x.Id.GetHashCode()) == -1;
+                return isMessageNew;
+            });
+        }
+    }
+}
