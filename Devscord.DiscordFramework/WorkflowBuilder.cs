16721957c209d3047f31a0c9972b6f0d220a9f45
Marcin Bigos <marcin.bigos99@gmail.com>
2020-03-23 19:31:28 +0100

    separate WorkflowBuilder

---
+++ Devscord.DiscordFramework/WorkflowBuilder.cs
@@ -0,0 +1,94 @@
+﻿using Autofac;
+using Devscord.DiscordFramework.Framework;
+using Devscord.DiscordFramework.Middlewares.Contexts;
+using Discord.WebSocket;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Devscord.DiscordFramework.Middlewares;
+
+namespace Devscord.DiscordFramework
+{
+    public class WorkflowBuilder
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly string _token;
+        private readonly IContainer _container;
+        private readonly Workflow _workflow;
+
+        private WorkflowBuilder(string token, IContainer container, Assembly botAssembly)
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                TotalShards = 1
+            });
+            this._token = token;
+            this._container = container;
+            this._workflow = new Workflow(botAssembly, container);
+            
+        }
+
+        public static WorkflowBuilder Create(string token, IContainer container, Assembly botAssembly) => new WorkflowBuilder(token, container, botAssembly);
+
+        public WorkflowBuilder SetMessageHandler(Func<SocketMessage, Task> action)
+        {
+            _client.MessageReceived += action;
+            return this;
+        }
+
+        public WorkflowBuilder SetDefaultMiddlewares()
+        {
+            this._workflow
+                .AddMiddleware<ChannelMiddleware, ChannelContext>()
+                .AddMiddleware<ServerMiddleware, DiscordServerContext>()
+                .AddMiddleware<UserMiddleware, UserContext>();
+            return this;
+        }
+
+        public WorkflowBuilder AddOnReadyHandlers(Action<WorkflowBuilderHandlers<Func<Task>>> action)
+        {
+            AddHandlers(action, this._workflow.OnReady);
+            return this;
+        }
+
+        public WorkflowBuilder AddOnUserJoinedHandlers(Action<WorkflowBuilderHandlers<Func<Contexts, Task>>> action)
+        {
+            AddHandlers(action, this._workflow.OnUserJoined);
+            return this;
+        }
+
+        public WorkflowBuilder AddOnMessageReceivedHandlers(Action<WorkflowBuilderHandlers<Func<SocketMessage, Task>>> action)
+        {
+            AddHandlers(action, this._workflow.OnMessageReceived);
+            return this;
+        }
+
+        public WorkflowBuilder AddOnWorkflowExceptionHandlers(Action<WorkflowBuilderHandlers<Action<Exception, Contexts>>> action)
+        {
+            AddHandlers(action, this._workflow.OnWorkflowException);
+            return this;
+        }
+
+        private void AddHandlers<T>(Action<WorkflowBuilderHandlers<T>> action, T workflowAction)
+        {
+            var workflowBuilderHandlers = new WorkflowBuilderHandlers<T>(this._container);
+            action.Invoke(workflowBuilderHandlers);
+            foreach (var exceptionHandler in workflowBuilderHandlers.Handlers)
+            {
+                workflowAction += (dynamic)exceptionHandler;
+            }
+        }
+
+        public async Task Run()
+        {
+            _workflow.Initialize();
+            _workflow.MapHandlers(_client);
+            ServerInitializer.Initialize(_client);
+
+            await _client.LoginAsync(TokenType.Bot, _token);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
