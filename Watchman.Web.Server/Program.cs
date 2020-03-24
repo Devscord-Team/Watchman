32bdd834525e71a4c352566af31b74068451af7b
Marcin Bigos <marcin.bigos99@gmail.com>
2020-03-24 22:35:39 +0100

    trying to fix autofac

--- Watchman.Web.Server/Program.cs
+++ Watchman.Web.Server/Program.cs
@@ -2,10 +2,14 @@ using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 using System.Threading.Tasks;
 using Watchman.Discord;
+using Watchman.Discord.IoC.Modules;
 using Watchman.Integrations.MongoDB;
+using Watchman.IoC.Modules;
 
 namespace Watchman.Web.Server
 {
@@ -14,14 +18,20 @@ namespace Watchman.Web.Server
         public static void Main(string[] args)
         {
             MongoConfiguration.Initialize();
+
             var configuration = GetConfiguration();
-            RunWatchmanBot(configuration);
-            CreateHostBuilder(args, configuration).Build().Run();
+            var containerBuilder = new ContainerBuilder();
+            ConfigureAutofac(containerBuilder, configuration); //todo optimalize to single instance of IoC
+            var container = containerBuilder.Build();
+            
+            RunWatchmanBot(configuration, container);
+            var builded = CreateHostBuilder(args, container).Build();
+            builded.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args, IConfiguration configuration) =>
+        public static IHostBuilder CreateHostBuilder(string[] args, IContainer container) =>
             Host.CreateDefaultBuilder(args)
-            .UseServiceProviderFactory(new AutofacServiceProviderFactory(x => ConfigureAutofac(x, configuration)))
+            
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
@@ -39,20 +49,35 @@ namespace Watchman.Web.Server
             return builder.Build();
         }
 
-        public static void RunWatchmanBot(IConfiguration configuration)
+        public static void RunWatchmanBot(IConfiguration configuration, IContainer container)
         {
             var discordConfiguration = new DiscordConfiguration
             {
                 Token = configuration["Discord:Token"],
                 MongoDbConnectionString = configuration.GetConnectionString("Mongo")
             };
-            var bot = new WatchmanBot(discordConfiguration, new Watchman.IoC.ContainerModule(configuration.GetConnectionString("Mongo")).GetBuilder().Build()); //todo optimalize to single instance of IoC
-            Task.Factory.StartNew(() => bot.Start(), TaskCreationOptions.LongRunning);
+            var workflowBuilder = new WatchmanBot(discordConfiguration, container).GetWorkflowBuilder();
+            Task.Run(() => workflowBuilder.Run());
         }
         private static void ConfigureAutofac(ContainerBuilder builder, IConfiguration configuration)
         {
-            var container = new Watchman.IoC.ContainerModule(configuration.GetConnectionString("Mongo"));
-            builder = container.GetBuilder();
+            RegisterModules(builder,
+                new Watchman.IoC.Modules.DatabaseModule(configuration.GetConnectionString("Mongo")),
+                new Watchman.IoC.Modules.CommandModule(),
+                new Watchman.IoC.Modules.QueryModule(),
+                new Watchman.IoC.Modules.ServiceModule(),
+                new Watchman.IoC.Modules.ControllerModule(),
+                new Watchman.Web.Server.IoC.ServerServiceModule());
+        }
+
+        private static void RegisterModules(ContainerBuilder builder, params Autofac.Module[] modules)
+        {
+            var method = typeof(Module).GetMethod("Load", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+            foreach (var module in modules)
+            {
+                method.Invoke(module, new object[] { builder });
+            }
         }
     }
 }
