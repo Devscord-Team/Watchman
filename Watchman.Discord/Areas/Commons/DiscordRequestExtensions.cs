4ea4efa7ee195ac970718b2ea3645bd61539e915
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-04-05 23:51:49 +0200

    fix formating exceptions

--- Watchman.Discord/Areas/Commons/DiscordRequestExtensions.cs
+++ Watchman.Discord/Areas/Commons/DiscordRequestExtensions.cs
@@ -50,7 +50,9 @@ namespace Watchman.Discord.Areas.Commons
             {
                 throw new TimeCannotBeNegativeException();
             }
-            if (timeAsNumber >= int.MaxValue)
+
+            // huge value will be too big for parsing to DateTime, so I use ushort (instead of int) to be sure that the value isn't too big
+            if (timeAsNumber >= ushort.MaxValue) 
             {
                 throw new TimeIsTooBigException();
             }
