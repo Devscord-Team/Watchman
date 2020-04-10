697ea04a8f9f98b926c0e70d2cb0cfc3ce29ec9c
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-04-10 14:20:52 +0200

    implement generating cyclic stats

--- Watchman.Discord/Areas/Statistics/Services/CyclicStatisticsGeneratorService.cs
+++ Watchman.Discord/Areas/Statistics/Services/CyclicStatisticsGeneratorService.cs
@@ -1,30 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Devscord.DiscordFramework.Services;
 using Watchman.Cqrs;
+using Watchman.DomainModel.Messages;
+using Watchman.DomainModel.Messages.Queries;
 
 namespace Watchman.Discord.Areas.Statistics.Services
 {
     public class CyclicStatisticsGeneratorService
     {
         private static bool _shouldStillGenerateEveryday;
+        private static bool _isNowRunningCyclicGenerator = false;
         private readonly IQueryBus _queryBys;
         private readonly ICommandBus _commandBus;
+        private readonly DiscordServersService _discordServersService;
 
-        public CyclicStatisticsGeneratorService(IQueryBus queryBys, ICommandBus commandBus)
+        public CyclicStatisticsGeneratorService(IQueryBus queryBys, ICommandBus commandBus, DiscordServersService discordServersService)
         {
             _queryBys = queryBys;
             _commandBus = commandBus;
+            _discordServersService = discordServersService;
         }
 
         public async Task StartGeneratingStatsCacheEveryday()
         {
+            if (_isNowRunningCyclicGenerator)
+            {
+                return;
+            }
+            _isNowRunningCyclicGenerator = true;
             while (_shouldStillGenerateEveryday)
             {
                 await BlockUntilNextNight();
                 await GenerateStatsForLastDay();
             }
+            _isNowRunningCyclicGenerator = false;
         }
 
         public Task StopGeneratingStatsCacheEveryday()
@@ -44,9 +57,17 @@ namespace Watchman.Discord.Areas.Statistics.Services
             await Task.Delay(delay);
         }
 
-        private Task GenerateStatsForLastDay()
+        private async Task GenerateStatsForLastDay()
         {
-            return Task.CompletedTask;
+            var servers = await _discordServersService.GetDiscordServers();
+            foreach (var server in servers)
+            {
+                var query = new GetMessagesQuery(server.Id);
+                //todo: filter one day
+                var messages = _queryBys.Execute(query).Messages.ToList();
+                var serverStatistic = new ServerDayStatistic(messages, server.Id);
+
+            }
         }
     }
 }
---
+++ Watchman.DomainModel/Messages/ServerDayStatistic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Watchman.Integrations.MongoDB;
+
+namespace Watchman.DomainModel.Messages
+{
+    public class ServerDayStatistic : Entity
+    {
+        public ulong ServerId { get; private set; }
+        public IEnumerable<ChannelDayStatistic> ChannelDayStatistics { get; private set; }
+        public int Count { get; private set; }
+
+        public ServerDayStatistic(IReadOnlyCollection<Message> serverMessages, ulong serverId)
+        {
+            ServerId = serverId;
+            Count = serverMessages.Count;
+            var channelsMessages = serverMessages.GroupBy(x => x.Channel.Id);
+
+            this.ChannelDayStatistics = channelsMessages.Select(x => new ChannelDayStatistic
+            {
+                ChannelId = x.Key,
+                Count = x.Count()
+            });
+        }
+    }
+
+    public class ChannelDayStatistic
+    {
+        public ulong ChannelId { get; set; }
+        public int Count { get; set; }
+    }
+}
