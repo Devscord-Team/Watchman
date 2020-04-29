c2037b0089cc6f04af27c917a20c5c02e67fa6e5
Marcin Bigos <marcin.bigos99@gmail.com>
2020-04-30 01:45:46 +0200

    ibotcommand

--- Devscord.DiscordFramework/Framework/Commands/IBotCommand.cs
+++ Devscord.DiscordFramework/Framework/Commands/IBotCommand.cs
@@ -7,15 +7,4 @@ namespace Devscord.DiscordFramework.Framework.Commands
     public interface IBotCommand
     {
     }
-
-    [AttributeUsage(AttributeTargets.Method)]
-    public class UseCommand : Attribute
-    {
-        public string CommandName { get; private set; }
-
-        public UseCommand(string commandName)
-        {
-            CommandName = commandName;
-        }
-    }
 }
