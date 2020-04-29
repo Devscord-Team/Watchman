9cfe3537dd8487c1f24b7191c9dfee486689faf4
Marcin Bigos <marcin.bigos99@gmail.com>
2020-04-30 01:43:00 +0200

    bot command property

---
+++ Devscord.DiscordFramework/Framework/Commands/Properties/BotCommandProperty.cs
@@ -0,0 +1,14 @@
+﻿namespace Devscord.DiscordFramework.Framework.Commands.Properties
+{
+    public class BotCommandProperty
+    {
+        public string Name { get; private set; }
+        public BotCommandPropertyType Type { get; private set; }
+
+        public BotCommandProperty(string name, BotCommandPropertyType type)
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+}
