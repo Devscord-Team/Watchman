09ba062e04bb08fc06b6e24da84cdcb7eab0f0f3
Adrian <Zilibdev@gmail.com>
2020-04-29 23:24:40 +0200

    test

--- Watchman.Discord/DiscordConfiguration.cs
+++ Watchman.Discord/DiscordConfiguration.cs
@@ -7,6 +7,7 @@ namespace Watchman.Discord
     public class DiscordConfiguration
     {
         public string Token { get; set; }
+
         public string MongoDbConnectionString { get; set; }
     }
 }
