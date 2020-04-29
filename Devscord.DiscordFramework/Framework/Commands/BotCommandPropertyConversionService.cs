c6d5c16c646a24080ce1ede190d302ef414f2671
Marcin Bigos <marcin.bigos99@gmail.com>
2020-04-30 01:45:59 +0200

    conversion service

---
+++ Devscord.DiscordFramework/Framework/Commands/BotCommandPropertyConversionService.cs
@@ -0,0 +1,36 @@
+﻿using Devscord.DiscordFramework.Framework.Commands.Properties;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Devscord.DiscordFramework.Framework.Commands
+{
+    public class BotCommandPropertyConversionService
+    {
+        private readonly Regex exTime = new Regex(@"(?<Value>\d+)(?<Unit>(h|m|s))", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        private readonly Regex exMention = new Regex(@"<@&?\d+>", RegexOptions.Compiled);
+
+        public object ConvertType(string value, BotCommandPropertyType type)
+        {
+            return type switch
+            {
+                BotCommandPropertyType.Time => this.ToTimeSpan(value),
+                BotCommandPropertyType.Number => int.Parse(value),//TODO add more types
+                _ => value
+            };
+        }
+
+        private TimeSpan ToTimeSpan(string value)
+        {
+            var match = exTime.Match(value);
+            var unit = match.Groups["Unit"].Value.ToLowerInvariant();
+            var timeValue = short.Parse(match.Groups["Value"].Value);
+            return unit switch
+            {
+                "h" => TimeSpan.FromHours(timeValue),
+                "m" => TimeSpan.FromMinutes(timeValue),
+                "s" => TimeSpan.FromSeconds(timeValue),
+                _ => throw new ArgumentNullException()
+            };
+        }
+    }
+}
