f05028ceb9a33a42e1819168da8451ec2a29ad01
Marcin Bigos <marcin.bigos99@gmail.com>
2020-04-30 19:05:19 +0200

    move code to services outside

---
+++ Devscord.DiscordFramework/Framework/Commands/Builders/BotCommandsTemplateBuilder.cs
@@ -0,0 +1,37 @@
+﻿using Devscord.DiscordFramework.Framework.Commands.Properties;
+using Devscord.DiscordFramework.Framework.Commands.PropertyAttributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Devscord.DiscordFramework.Framework.Commands.Builders
+{
+    public class BotCommandsTemplateBuilder
+    {
+        public BotCommandTemplate GetCommandTemplate(Type commandType)
+        {
+            var properties = GetBotCommandProperties(commandType);
+            var template = new BotCommandTemplate(commandType.Name, properties);
+            return template;
+        }
+
+        private IEnumerable<BotCommandProperty> GetBotCommandProperties(Type commandType)
+        {
+            foreach (var commandProperty in commandType.GetProperties())
+            {
+                yield return GetBotCommandProperty(commandProperty);
+            }
+        }
+
+        private BotCommandProperty GetBotCommandProperty(PropertyInfo commandProperty)
+        {
+            var name = commandProperty.Name;
+            var attributes = commandProperty.GetCustomAttributes(typeof(CommandPropertyAttribute), true).Select(x => x as CommandPropertyAttribute);
+            var attribute = attributes.FirstOrDefault(x => !(x is Optional)) ?? new SingleWord();
+            var type = (BotCommandPropertyType)Enum.Parse(typeof(BotCommandPropertyType), attribute.GetType().Name);
+            var isOptional = attributes.Any(x => x is Optional);
+            return new BotCommandProperty(name, type, isOptional);
+        }
+    }
+}
---
+++ Devscord.DiscordFramework/Framework/Commands/Services/BotCommandsParsingService.cs
@@ -0,0 +1,29 @@
+﻿using Devscord.DiscordFramework.Framework.Commands.Parsing.Models;
+using System;
+using System.Linq;
+
+namespace Devscord.DiscordFramework.Framework.Commands.Services
+{
+    public class BotCommandsParsingService
+    {
+        private readonly BotCommandsPropertyConversionService botCommandPropertyConversionService;
+
+        public BotCommandsParsingService(BotCommandsPropertyConversionService botCommandPropertyConversionService)
+        {
+            this.botCommandPropertyConversionService = botCommandPropertyConversionService;
+        }
+
+        public IBotCommand ParseRequestToCommand(Type commandType, DiscordRequest request, BotCommandTemplate template)
+        {
+            var instance = Activator.CreateInstance(commandType);
+            foreach (var property in commandType.GetProperties())
+            {
+                var value = request.Arguments.First(x => x.Name.ToLowerInvariant() == property.Name.ToLowerInvariant()).Value;
+                var propertyType = template.Properties.First(x => x.Name == property.Name).Type;
+                var convertedType = botCommandPropertyConversionService.ConvertType(value, propertyType);
+                property.SetValue(instance, convertedType);
+            }
+            return (IBotCommand)instance;
+        }
+    }
+}
