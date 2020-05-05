3d546a8f6698d7a80cecc8e6e87858a3f36fd814
ukas <lukasz@ukasup.pl>
2020-05-05 21:49:50 +0200

    changed names inside MessageDto

--- Watchman.Web.Server/Areas/Messages/Models/Dtos/MessageDto.cs
+++ Watchman.Web.Server/Areas/Messages/Models/Dtos/MessageDto.cs
@@ -9,9 +9,9 @@ namespace Watchman.Web.Server.Areas.Messages.Models.Dtos
     public class MessageDto
     {
         public string Content { get; private set; }
-        public MessageUserDto UserDto { get; private set; }
-        public MessageChannelDto ChannelDto { get; private set; }
-        public MessageServerDto ServerDto { get; private set; }
+        public MessageUserDto User { get; private set; }
+        public MessageChannelDto Channel { get; private set; }
+        public MessageServerDto Server { get; private set; }
         public DateTime SentAt { get; private set; }
 
         public MessageDto()
@@ -21,9 +21,9 @@ namespace Watchman.Web.Server.Areas.Messages.Models.Dtos
         public MessageDto(Message message)
         {
             Content = message.Content;
-            UserDto = new MessageUserDto(message.Author.Id, message.Author.Name);
-            ChannelDto = new MessageChannelDto(message.Channel.Id, message.Channel.Name);
-            ServerDto = new MessageServerDto(message.Server.Id, message.Server.Name, message.Server.Owner.Id, message.Server.Owner.Name);
+            User = new MessageUserDto(message.Author.Id, message.Author.Name);
+            Channel = new MessageChannelDto(message.Channel.Id, message.Channel.Name);
+            Server = new MessageServerDto(message.Server.Id, message.Server.Name, message.Server.Owner.Id, message.Server.Owner.Name);
             SentAt = message.SentAt;
         }
     }
