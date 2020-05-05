db1c5befe904196feecc2d3bd3ea1062fa22d4c4
ukas <lukasz@ukasup.pl>
2020-05-05 19:53:40 +0200

    added Messages controller with GetMessage method

---
+++ Watchman.Web.Server/Areas/Messages/Controllers/MessagesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Watchman.Cqrs;
+using Watchman.DomainModel.Messages;
+using Watchman.DomainModel.Messages.Queries;
+using Watchman.DomainModel.Responses.Commands;
+using Watchman.DomainModel.Responses.Queries;
+using Watchman.Web.Server.Areas.Commons.Integration;
+using Watchman.Web.Server.Areas.Messages.Models.Dtos;
+using Watchman.Web.Server.Areas.Responses.Models.Dtos;
+
+namespace Watchman.Web.Server.Areas.Messages.Controllers
+{
+    public class MessagesController : BaseApiController
+    {
+        private readonly IQueryBus _queryBus;
+
+        public MessagesController(IQueryBus queryBus)
+        {
+            this._queryBus = queryBus;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<MessageDto>> GetMessages(ulong serverId, ulong channelId,  ulong? userId)
+        {
+            var query = new GetMessagesQuery(serverId);
+            var responseResult = await _queryBus.ExecuteAsync(query);
+            IEnumerable<MessageDto> messagesDto;
+            if (userId.HasValue)
+            {
+                messagesDto = responseResult.Messages.Where(x => x.Author.Id == userId && x.Channel.Id == channelId).Take(100).Select(x => new MessageDto(x));
+            }
+            else
+            {
+                messagesDto = responseResult.Messages.Where(x => x.Channel.Id == channelId).Take(100).Select(x => new MessageDto(x));
+            }
+            return messagesDto;
+        }
+    }
+}
---
+++ Watchman.Web.Server/Areas/Messages/Models/Dtos/MessageDto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Watchman.DomainModel.Messages;
+
+namespace Watchman.Web.Server.Areas.Messages.Models.Dtos
+{
+    public class MessageDto
+    {
+        public string Content { get; private set; }
+        public ulong AuthorId { get; private set; }
+        public string AuthorName { get; private set; }
+        public ulong ChannelId { get; private set; }
+        public string ChannelName { get; private set; }
+        public ulong ServerId { get; private set; }
+        public string ServerName { get; private set; }
+        public ulong ServerOwnerId { get; private set; }
+        public string ServerOwnerName { get; private set; }
+        public DateTime SentAt { get; private set; }
+
+        public MessageDto()
+        {
+        }
+
+        public MessageDto(Message message)
+        {
+            Content = message.Content;
+            AuthorId = message.Author.Id;
+            AuthorName = message.Author.Name;
+            ChannelId = message.Channel.Id;
+            ChannelName = message.Channel.Name;
+            ServerId = message.Server.Id;
+            ServerName = message.Server.Name;
+            ServerOwnerId = message.Server.Owner.Id;
+            ServerOwnerName = message.Server.Owner.Name;
+            SentAt = message.SentAt;
+        }
+    }
+}
