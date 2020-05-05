d6676a2fd15b5c38bc884a0c8231dc117bd06b4b
ukas <lukasz@ukasup.pl>
2020-05-05 22:11:15 +0200

    splitted up MessageDto objects

--- Watchman.Web.Server/Areas/Messages/Models/Dtos/MessageDto.cs
+++ Watchman.Web.Server/Areas/Messages/Models/Dtos/MessageDto.cs
@@ -23,47 +23,8 @@ namespace Watchman.Web.Server.Areas.Messages.Models.Dtos
             Content = message.Content;
             User = new MessageUserDto(message.Author.Id, message.Author.Name);
             Channel = new MessageChannelDto(message.Channel.Id, message.Channel.Name);
-            Server = new MessageServerDto(message.Server.Id, message.Server.Name, message.Server.Owner.Id, message.Server.Owner.Name);
+            Server = new MessageServerDto(message.Server.Id, message.Server.Name, new MessageServerOwnerDto(message.Server.Owner.Id, message.Server.Owner.Name));
             SentAt = message.SentAt;
-        }
-    }
-    public class MessageUserDto
-    {
-        public ulong Id { get; private set; }
-        public string Name { get; private set; }
-
-        public MessageUserDto(ulong id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
-    }
-
-    public class MessageServerDto
-    {
-        public ulong Id { get; private set; }
-        public string Name { get; private set; }
-        public ulong OwnerId { get; private set; }
-        public string OwnerName { get; private set; }
-
-        public MessageServerDto(ulong id, string name, ulong ownerId, string ownerName)
-        {
-            Id = id;
-            Name = name;
-            OwnerId = ownerId;
-            OwnerName = ownerName;
-        }
-    }
-
-    public class MessageChannelDto
-    {
-        public ulong Id { get; private set; }
-        public string Name { get; private set; }
-
-        public MessageChannelDto(ulong id, string name)
-        {
-            Id = id;
-            Name = name;
         }
     }
 }
---
+++ Watchman.Web.Server/Areas/Messages/Models/Dtos/MessageServerOwnerDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Watchman.Web.Server.Areas.Messages.Models.Dtos
+{
+    public class MessageServerOwnerDto
+    {
+        public ulong Id { get; private set; }
+        public string Name { get; private set; }
+
+        public MessageServerOwnerDto(ulong id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
