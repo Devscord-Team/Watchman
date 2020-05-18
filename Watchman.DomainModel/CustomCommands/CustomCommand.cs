f3ae4c867e38bde9bea64ab8f01bdcd3a892c359
Marcin Bigos <marcin.bigos99@gmail.com>
2020-05-18 18:42:57 +0200

    add custom command

---
+++ Watchman.DomainModel/CustomCommands/CustomCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Watchman.Integrations.MongoDB;
+
+namespace Watchman.DomainModel.CustomCommands
+{
+    public class CustomCommand : Entity, IAggregateRoot
+    {
+    }
+}
