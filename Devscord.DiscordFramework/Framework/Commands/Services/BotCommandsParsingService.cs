e9f1c44cca2bdf852559af79f7da4c2a94e7418d
Marcin Bigos <marcin.bigos99@gmail.com>
2020-05-18 19:49:13 +0200

    move validation outside

--- Devscord.DiscordFramework/Framework/Commands/Services/BotCommandsParsingService.cs
+++ Devscord.DiscordFramework/Framework/Commands/Services/BotCommandsParsingService.cs
@@ -31,14 +31,30 @@ namespace Devscord.DiscordFramework.Framework.Commands.Services
         public IBotCommand ParseCustomTemplate(Type commandType, BotCommandTemplate template, Regex customTemplate, string input)
         {
             var match = customTemplate.Match(input);
-            if(!match.Success 
-                || match.Groups.Count < template.Properties.Where(x => !x.IsOptional).Count()
-                || template.Properties.Where(x => !x.IsOptional).Any(x => !match.Groups.ContainsKey(x.Name)))
+            if(this.CustomTemplateIsValid(match, template))
             {
                 Log.Warning("Custom template {customTemplate} is not valid for {commandName}", customTemplate, template.CommandName);
                 return null;
             }
             var instance = Activator.CreateInstance(commandType);
+        }
+
+        private bool CustomTemplateIsValid(Match match, BotCommandTemplate template)
+        {
+            if(!match.Success)
+            {
+                return false;
+            }
+            var requiredProperties = template.Properties.Where(x => !x.IsOptional).ToList();
+            if (match.Groups.Count < requiredProperties.Count)
+            {
+                return false;
+            }
+            if(requiredProperties.Any(x => !match.Groups.ContainsKey(x.Name)))
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
