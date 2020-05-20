0681d037631de757f2d9557366670cf97b867047
Marcin Bigos <marcin.bigos99@gmail.com>
2020-05-20 21:22:18 +0200

    add and delete command handlers

---
+++ Watchman.DomainModel/CustomCommands/Commands/Handlers/AddCustomCommandsCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Watchman.Cqrs;
+using Watchman.Integrations.MongoDB;
+
+namespace Watchman.DomainModel.CustomCommands.Commands.Handlers
+{
+    public class AddCustomCommandsCommandHandler : ICommandHandler<AddCustomCommandsCommand>
+    {
+        private readonly ISessionFactory sessionFactory;
+
+        public AddCustomCommandsCommandHandler(ISessionFactory sessionFactory)
+        {
+            this.sessionFactory = sessionFactory;
+        }
+
+        public async Task HandleAsync(AddCustomCommandsCommand command)
+        {
+            using var session = this.sessionFactory.Create();
+            var customCommand = new CustomCommand(command.CommandFullName, command.CustomTemplateRegex, command.ServerId);
+            await session.AddAsync(customCommand);
+        }
+    }
+}
