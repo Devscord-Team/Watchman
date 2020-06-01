7d80db72685b7f23215285c6b399d53f2fc8823d
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-06-01 17:50:41 +0200

    add caching messages

---
+++ Devscord.DiscordFramework/Framework/Commands/AntiSpam/Models/ServerSmallMessages.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Devscord.DiscordFramework.Framework.Commands.Parsing.Models;
+using Devscord.DiscordFramework.Middlewares.Contexts;
+using Devscord.DiscordFramework.Services.Models;
+
+namespace Devscord.DiscordFramework.Framework.Commands.AntiSpam.Models
+{
+    public class ServerSmallMessages
+    {
+        private static Dictionary<ulong, List<SmallMessage>> _usersMessages;
+
+        static ServerSmallMessages()
+        {
+            _usersMessages = new Dictionary<ulong, List<SmallMessage>>();
+            RemoveOldMessagesCyclic();
+        }
+
+        public ServerSmallMessages()
+        {
+        }
+
+        public void OverwriteMessages(IEnumerable<SmallMessage> smallMessages)
+        {
+            _usersMessages = smallMessages.GroupBy(x => x.UserId).ToDictionary(x => x.Key, x => x.ToList());
+        }
+
+        public void AddMessage(SmallMessage smallMessage)
+        {
+            _usersMessages[smallMessage.UserId].Add(smallMessage);
+        }
+
+        public void AddMessage(DiscordRequest request, Contexts contexts)
+        {
+            var smallMessage = new SmallMessage(request.OriginalMessage, contexts.User.Id, request.SentAt);
+            this.AddMessage(smallMessage);
+        }
+
+        public Dictionary<ulong, List<SmallMessage>> GetLastMessagesPaired()
+        {
+            return _usersMessages;
+        }
+
+        public List<SmallMessage> GetLastUserMessages(ulong userId)
+        {
+            return _usersMessages[userId];
+        }
+
+        private static async void RemoveOldMessagesCyclic()
+        {
+            await Task.Delay(TimeSpan.FromMinutes(2));
+            var minTimeInPast = DateTime.Now.AddMinutes(-5);
+            _usersMessages = _usersMessages.Values.Select(list =>
+            {
+                list.RemoveAll(message => message.SentAt < minTimeInPast);
+                return list;
+            }).ToDictionary(x => x.First().UserId, x => x);
+        }
+    }
+}
