0d98901592ee3cc6fe4f7f8255531295eb3a95b8
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-06-07 10:08:38 +0200

    add tests for overallDetector

--- Watchman.Discord.UnitTests/AntiSpamDetectionTests.cs
+++ Watchman.Discord.UnitTests/AntiSpamDetectionTests.cs
@@ -138,31 +138,73 @@ namespace Watchman.Discord.UnitTests
         }
 
         [Test]
-        [TestCase()]
-        public void OverallSpamDetectorStrategy_ShouldDetectSpam()
+        [TestCase("spam", "spam", "spam", "spam", 1000, SpamProbability.Medium)]
+        [TestCase("aaaaaaaaaabbbbb", "https://discord.gg/example", "https://discord.gg/example", "https://discord.gg/example", 20, SpamProbability.Sure)]
+        [TestCase("link: https://discord.gg/example", "https://discord.gg/example", "https://discord.gg/example", "https://discord.gg/example", 20, SpamProbability.Sure)]
+        public void OverallSpamDetectorStrategy_ShouldDetectSpam(string messageContent1, string messageContent2, string messageContent3, string messageContent4, int userMessagesCount, SpamProbability exceptedSpamProbability)
         {
             // Arrange
-            //todo: write tests
+                _userMessagesCounter
+                .Setup(x => x.CountUserMessages(DEFAULT_TEST_USER_ID, GetMessagesQuery.GET_ALL_SERVERS))
+                .Returns(userMessagesCount);
+            _userMessagesCounter
+                .Setup(x => x.UserMessagesCountToBeSafe)
+                .Returns(DEFAULT_COUNT_TO_BE_SAFE);
+
+            var lastMessage = CreateMessage(messageContent4);
+            var serverMessages = new ServerMessagesCacheService();
+            serverMessages.OverwriteMessages(new List<SmallMessage>
+            {
+                new SmallMessage(messageContent1, DEFAULT_TEST_USER_ID, DateTime.Now),
+                new SmallMessage(messageContent2, DEFAULT_TEST_USER_ID, DateTime.Now),
+                new SmallMessage(messageContent3, DEFAULT_TEST_USER_ID, DateTime.Now)
+            });
+            var spamDetectors = new List<ISpamDetector>
+            {
+                new LinksDetectorStrategy(_userMessagesCounter.Object),
+                new DuplicatedMessagesDetectorStrategy(_userMessagesCounter.Object)
+            };
+            var overallSpamDetector = new OverallSpamDetectorStrategy(serverMessages, spamDetectors);
 
             // Act
-
+            var overallSpamProbability = overallSpamDetector.GetOverallSpamProbability(lastMessage);
 
             // Assert
-            Assert.Fail();
+            Assert.That(overallSpamProbability, Is.EqualTo(exceptedSpamProbability));
         }
 
         [Test]
-        [TestCase()]
-        public void OverallSpamDetectorStrategy_ShouldNotDetectSpam()
+        [TestCase("not spam", "something else", "something more else", "totally different", 10)]
+        public void OverallSpamDetectorStrategy_ShouldNotDetectSpam(string messageContent1, string messageContent2, string messageContent3, string messageContent4, int userMessagesCount)
         {
             // Arrange
-            //todo: write tests
+            _userMessagesCounter
+            .Setup(x => x.CountUserMessages(DEFAULT_TEST_USER_ID, GetMessagesQuery.GET_ALL_SERVERS))
+            .Returns(userMessagesCount);
+            _userMessagesCounter
+                .Setup(x => x.UserMessagesCountToBeSafe)
+                .Returns(DEFAULT_COUNT_TO_BE_SAFE);
+
+            var lastMessage = CreateMessage(messageContent4);
+            var serverMessages = new ServerMessagesCacheService();
+            serverMessages.OverwriteMessages(new List<SmallMessage>
+            {
+                new SmallMessage(messageContent1, DEFAULT_TEST_USER_ID, DateTime.Now),
+                new SmallMessage(messageContent2, DEFAULT_TEST_USER_ID, DateTime.Now),
+                new SmallMessage(messageContent3, DEFAULT_TEST_USER_ID, DateTime.Now)
+            });
+            var spamDetectors = new List<ISpamDetector>
+            {
+                new LinksDetectorStrategy(_userMessagesCounter.Object),
+                new DuplicatedMessagesDetectorStrategy(_userMessagesCounter.Object)
+            };
+            var overallSpamDetector = new OverallSpamDetectorStrategy(serverMessages, spamDetectors);
 
             // Act
-
+            var overallSpamProbability = overallSpamDetector.GetOverallSpamProbability(lastMessage);
 
             // Assert
-            Assert.Fail();
+            Assert.That(overallSpamProbability, Is.EqualTo(SpamProbability.None));
         }
 
         private Message CreateMessage(string content)
