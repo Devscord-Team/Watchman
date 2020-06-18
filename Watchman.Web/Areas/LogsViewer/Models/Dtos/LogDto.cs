b603269f42dd95b423575d7421b735b66ad115b2
Marcin Bigos <marcin.bigos99@gmail.com>
2020-06-18 23:16:13 +0200

    working get logs

--- Watchman.Web/Areas/LogsViewer/Models/Dtos/LogDto.cs
+++ Watchman.Web/Areas/LogsViewer/Models/Dtos/LogDto.cs
@@ -5,12 +5,16 @@ using System.Threading.Tasks;
 
 namespace Watchman.Web.Areas.LogsViewer.Models.Dtos
 {
+    public class LogsDto
+    {
+        public LogDto[] Logs { get; set; }
+    }
+
     public class LogDto
     {
         public DateTime Timestamp { get; set; }
         public string Level { get; set; }
         public string MessageTemplate { get; set; }
-        public dynamic[] Properties { get; set; }
+        public Dictionary<string, object> Properties { get; set; }
     }
-
 }
--- Watchman.Web/Areas/LogsViewer/Services/LogsService.cs
+++ Watchman.Web/Areas/LogsViewer/Services/LogsService.cs
@@ -1,8 +1,12 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Serilog;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Watchman.Web.Areas.LogsViewer.Models.Dtos;
@@ -15,17 +19,19 @@ namespace Watchman.Web.Areas.LogsViewer.Services
         {
             foreach (var filePath in Directory.GetFiles("logs").Where(x => Path.GetExtension(x) == ".json"))
             {
-                using (var reader = new StreamReader(filePath, Encoding.UTF8))
+                using var stream = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                using var streamReader = new StreamReader(stream);
+
+                var content = streamReader.ReadToEnd();
+                var normalizedContent = "[" + content.Trim(",[]".ToCharArray()) + "]";
+                var logs = JsonConvert.DeserializeObject<LogDto[]>(normalizedContent);
+
+                foreach (var log in logs)
                 {
-                    var content = reader.ReadToEnd();
-                    var normalizedContent = $"[{content}]";
-                    var logs = JsonConvert.DeserializeObject<IEnumerable<LogDto>>(content);
-                    foreach (var log in logs)
-                    {
-                        yield return log;
-                    }
+                    yield return log;
                 }
             }
         }
     }
 }
+
