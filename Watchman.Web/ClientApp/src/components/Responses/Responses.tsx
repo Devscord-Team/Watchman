2c856ea9b90dda0ffab4a56065bc3bdfcd049109
Marcin Bigos <marcin.bigos99@gmail.com>
2020-06-18 20:13:59 +0200

    codefactor

--- Watchman.Web/ClientApp/src/components/Responses/Responses.tsx
+++ Watchman.Web/ClientApp/src/components/Responses/Responses.tsx
@@ -1,9 +1,5 @@
 import * as React from "react";
 
-export interface IResponsesProps {}
-
-export interface IResponsesState {}
-
 export default class Responses extends React.Component<{}, {}> {
     public render() {
         return <div></div>;
