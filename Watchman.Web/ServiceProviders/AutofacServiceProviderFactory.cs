2d9e5cf1fcee503cb601cd0c97230c3d2d9134aa
Marcin Bigos <marcin.bigos99@gmail.com>
2020-06-18 20:12:19 +0200

    save

---
+++ Watchman.Web/ServiceProviders/AutofacServiceProviderFactory.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Watchman.Discord;
+using Watchman.IoC;
+
+namespace Watchman.Web.ServiceProviders
+{
+    public class AutofacServiceProviderFactory : IServiceProviderFactory<ContainerBuilder>
+    {
+        private readonly IConfiguration configuration;
+
+        public AutofacServiceProviderFactory(IConfiguration configuration) => this.configuration = configuration;
+
+        public ContainerBuilder CreateBuilder(IServiceCollection services)
+        {
+            var containerModule = new ContainerModule(this.configuration.GetConnectionString("Mongo"));
+            var builder = containerModule.GetBuilder();
+            builder.Populate(services);
+            return builder;
+        }
+
+        public IServiceProvider CreateServiceProvider(ContainerBuilder containerBuilder)
+        {
+            var container = containerBuilder.Build();
+            var configuration = container.Resolve<IConfiguration>();
+
+            var watchman = new WatchmanBot(new DiscordConfiguration
+            {
+                MongoDbConnectionString = configuration.GetConnectionString("Mongo"),
+                Token = configuration["Discord:Token"]
+            }, container.Resolve<IComponentContext>()).GetWorkflowBuilder();
+
+            return new AutofacServiceProvider(container);
+        }
+    }
+}
