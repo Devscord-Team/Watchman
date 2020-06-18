2d72dd0dbfaa43856eb80619b34ba9534330c7fc
Marcin Bigos <marcin.bigos99@gmail.com>
2020-06-18 20:09:58 +0200

    codefactor

--- Watchman.Web/Startup.cs
+++ Watchman.Web/Startup.cs
@@ -33,7 +33,6 @@ namespace Watchman.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
