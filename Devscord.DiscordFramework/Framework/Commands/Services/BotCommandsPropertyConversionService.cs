382599cc9fdd5fdb0e4472e83b1fc37bcc950f0f
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-07-02 00:23:59 +0200

    remove time with point

--- Devscord.DiscordFramework/Framework/Commands/Services/BotCommandsPropertyConversionService.cs
+++ Devscord.DiscordFramework/Framework/Commands/Services/BotCommandsPropertyConversionService.cs
@@ -7,7 +7,7 @@ namespace Devscord.DiscordFramework.Framework.Commands.Services
 {
     public class BotCommandsPropertyConversionService
     {
-        private readonly Regex _exTime = new Regex(@"(?<Value>[\d\.,]+)(?<Unit>(ms|d|h|m|s))", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        private readonly Regex _exTime = new Regex(@"(?<Value>\d+)(?<Unit>(ms|d|h|m|s))", RegexOptions.IgnoreCase | RegexOptions.Compiled);
         private readonly Regex _exMention = new Regex(@"<@&?\d+>", RegexOptions.Compiled);
 
         public object ConvertType(string value, BotCommandPropertyType type)
@@ -25,7 +25,7 @@ namespace Devscord.DiscordFramework.Framework.Commands.Services
         {
             var match = this._exTime.Match(value);
             var unit = match.Groups["Unit"].Value.ToLowerInvariant();
-            var timeValue = double.Parse(match.Groups["Value"].Value.Replace(',', '.'), CultureInfo.InvariantCulture);
+            var timeValue = uint.Parse(match.Groups["Value"].Value);
             return unit switch
             {
                 "d" => TimeSpan.FromDays(timeValue),
