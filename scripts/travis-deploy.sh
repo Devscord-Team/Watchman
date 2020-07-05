15c2b36dc174c0b9f9fcee103bda06d960934929
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-07-05 21:26:44 +0200

    add apt update

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -1,7 +1,8 @@
 docker build -t $DOCKERHUB_LOGIN/watchman .
 docker login --username $DOCKERHUB_LOGIN --password $DOCKERHUB_PASS
 docker push $DOCKERHUB_LOGIN/watchman:latest
-sudo apt-get install sshpass
+sudo apt update
+sudo apt install sshpass
 sshpass -p $SERVER_PASS ssh $SERVER_ADRESS
 curl -o docker-compose.yml https://raw.githubusercontent.com/Devscord-Team/Watchman/master/docker-compose.yml
 docker-compose up -d
