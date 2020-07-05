cbb71974e4943226217450f036e6b7617be801e9
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-07-06 00:08:14 +0200

    fix

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -1,6 +1,6 @@
 docker build -t $DOCKERHUB_LOGIN/watchman .
-docker login --password $DOCKERHUB_PASS --username $DOCKERHUB_LOGIN
+docker login -p $DOCKERHUB_PASS -u $DOCKERHUB_LOGIN
 docker push $DOCKERHUB_LOGIN/watchman:latest
-ssh $SSH_USER@$SERVER_ADDRESS
+ssh $SSH_USER@$SERVER_ADDRESS -i /tmp/deploy_rsa
 curl -o docker-compose.yml https://raw.githubusercontent.com/Devscord-Team/Watchman/master/docker-compose.yml
 docker-compose up -d
