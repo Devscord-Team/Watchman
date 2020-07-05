d3ed1a9d5602cff8773d016abe0c23cd0496297f
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-07-05 20:19:26 +0200

    add auto deployment

---
+++ scripts/travis-deploy.sh
@@ -0,0 +1,8 @@
+docker build -t DOCKERHUB_LOGIN/watchman .
+docker login --username DOCKERHUB_LOGIN
+DOCKERHUB_PASS
+docker push gagyn/watchman:latest
+ssh SERVER_ADRESS
+SERVER_PASS
+curl -o docker-compose.yml https://raw.githubusercontent.com/Devscord-Team/Watchman/master/docker-compose.yml
+docker-compose up -d
