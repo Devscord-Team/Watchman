7c6781ed066c2caf8c786c9b8d2baa05978ef122
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-07-05 21:14:23 +0200

    fix deploy

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -1,8 +1,7 @@
 docker build -t $DOCKERHUB_LOGIN/watchman .
-docker login --username $DOCKERHUB_LOGIN
-$DOCKERHUB_PASS
-docker push gagyn/watchman:latest
-ssh $SERVER_ADRESS
-$SERVER_PASS
+docker login --username $DOCKERHUB_LOGIN --password $DOCKERHUB_PASS
+docker push $DOCKERHUB_LOGIN/watchman:latest
+sudo apt-get install sshpass
+sshpass -p $SERVER_PASS ssh $SERVER_ADRESS
 curl -o docker-compose.yml https://raw.githubusercontent.com/Devscord-Team/Watchman/master/docker-compose.yml
 docker-compose up -d
