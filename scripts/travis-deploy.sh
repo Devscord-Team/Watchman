dbc01812561f86c87812b6035f8f7bfff04019f1
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-07-06 16:09:58 +0200

    add docker-compose down

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -2,4 +2,4 @@ docker build -t $DOCKERHUB_LOGIN/watchman .
 docker login -p $DOCKERHUB_PASS -u $DOCKERHUB_LOGIN
 docker push $DOCKERHUB_LOGIN/watchman:latest
 ssh-keyscan -H $SERVER_ADDRESS >> ~/.ssh/known_hosts
-ssh $SSH_USER@$SERVER_ADDRESS -i /tmp/deploy_rsa 'curl -o docker-compose.yml https://raw.githubusercontent.com/Devscord-Team/Watchman/master/docker-compose.yml && docker-compose up -d && exit'
+ssh $SSH_USER@$SERVER_ADDRESS -i /tmp/deploy_rsa 'curl -o docker-compose.yml https://raw.githubusercontent.com/Devscord-Team/Watchman/master/docker-compose.yml && docker-compose down && docker-compose up -d && exit'
