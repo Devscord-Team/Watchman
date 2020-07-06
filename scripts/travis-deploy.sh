a971125acb818d916f94adf8dcc5ac1e46edbef8
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-07-06 13:09:57 +0200

    try to fix passing vars

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -1,6 +1,6 @@
-docker build -t $DOCKERHUB_LOGIN/watchman .
-docker login -p $DOCKERHUB_PASS -u $DOCKERHUB_LOGIN
-docker push $DOCKERHUB_LOGIN/watchman:latest
-ssh $SSH_USER@$SERVER_ADDRESS -i /tmp/deploy_rsa
+docker build -t $(DOCKERHUB_LOGIN)\/watchman .
+docker login -p $(DOCKERHUB_PASS) -u $(DOCKERHUB_LOGIN)
+docker push $(DOCKERHUB_LOGIN)\/watchman:latest
+ssh $(SSH_USER)@$(SERVER_ADDRESS) -i /tmp/deploy_rsa
 curl -o docker-compose.yml https://raw.githubusercontent.com/Devscord-Team/Watchman/master/docker-compose.yml
 docker-compose up -d
