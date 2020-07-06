7496dfff0929cfb1bbf9730a4cb33b46c3e69544
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-07-06 14:22:58 +0200

    deploy only on master

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -1,4 +1,4 @@
-docker build -t "$DOCKERHUB_LOGIN"\/watchman .
+docker build -t $DOCKERHUB_LOGIN\/watchman .
 docker login -p $DOCKERHUB_PASS -u $DOCKERHUB_LOGIN
 docker push $DOCKERHUB_LOGIN\/watchman:latest
 ssh $SSH_USER\@$SERVER_ADDRESS -i /tmp/deploy_rsa
