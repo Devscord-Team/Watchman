fa6e8a3251a2c3861f9eddd3ded21ae75a081a06
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-07-12 22:43:40 +0200

    always assign muted role to channels

--- Watchman.Discord/Program.cs
+++ Watchman.Discord/Program.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using Newtonsoft.Json;
-using Serilog;
+﻿using Newtonsoft.Json;
 using System.IO;
 using System.Threading.Tasks;
-using Devscord.DiscordFramework;
 
 namespace Watchman.Discord
 {
     internal class Program
     {
-        private const int HOW_MANY_EXCEPTIONS_IN_SHORT_TIME_TO_STOP_BOT = 4;
-        private static int _numberOfExceptionsInLastTime;
-        private static DateTime _lastException = DateTime.MinValue;
-
         public static async Task Main(string[] args)
         {
 #if DEBUG
@@ -24,44 +17,8 @@ namespace Watchman.Discord
 
             var watchman = new WatchmanBot(configuration);
             var workflowBuilder = watchman.GetWorkflowBuilder();
-
-            AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
-            {
-                HandleException((Exception) e.ExceptionObject);
-            };
-            while (true)
-            {
-                TryToRun(workflowBuilder).Wait();
-            }
-        }
-
-        private static async Task TryToRun(WorkflowBuilder workflowBuilder)
-        {
-            if (_numberOfExceptionsInLastTime++ >= HOW_MANY_EXCEPTIONS_IN_SHORT_TIME_TO_STOP_BOT)
-            {
-                throw new Exception("Too many restarts");
-            }
-            try
-            {
-                workflowBuilder.Build();
-                await Task.Delay(-1);
-            }
-            catch (Exception e)
-            {
-                HandleException(e);
-                await Task.Delay(10000);
-            }
-        }
-
-        private static void HandleException(Exception e)
-        {
-            Log.Error(e, "Not handled exception!");
-
-            if (_lastException < DateTime.Now.AddMinutes(-30))
-            {
-                _numberOfExceptionsInLastTime = 0;
-            }
-            _lastException = DateTime.Now;
+            workflowBuilder.Build();
+            await Task.Delay(-1);
         }
     }
 }
