d18523e644bd0dc7486159495b0dea0de4518253
Marcin Bigos <marcin.bigos99@gmail.com>
2020-07-15 19:54:30 +0200

    fix logs

--- Devscord.DiscordFramework/ControllersContainer.cs
+++ Devscord.DiscordFramework/ControllersContainer.cs
@@ -18,17 +18,17 @@ namespace Devscord.DiscordFramework
             this.WithReadAlways = controllers
                 .Select(x => new ControllerInfo(x.Controller, x.Methods.Where(m => m.HasAttribute<ReadAlways>())))
                 .Where(x => x.Methods.Any()).ToList();
-            Log.Debug("Found {quantity} ReadAlwayd controllers", this.WithDiscordCommand.Count());
+            Log.Debug("Found {quantity} ReadAlwayd methods", this.WithReadAlways == null ? 0 : this.WithReadAlways.SelectMany(x => x.Methods).Count());
 
             this.WithDiscordCommand = controllers
                 .Select(x => new ControllerInfo(x.Controller, x.Methods.Where(m => m.HasAttribute<DiscordCommand>())))
                 .Where(x => x.Methods.Any()).ToList();
-            Log.Debug("Found {quantity} DiscordCommand controllers", this.WithDiscordCommand.Count());
+            Log.Debug("Found {quantity} DiscordCommand methods", this.WithDiscordCommand == null ? 0 : this.WithDiscordCommand.SelectMany(x => x.Methods).Count());
 
             this.WithIBotCommand = controllers
                 .Select(x => new ControllerInfo(x.Controller, x.Methods.Where(x => x.HasParameter<IBotCommand>())))
                 .Where(x => x.Methods.Any()).ToList();
-            Log.Debug("Found {quantity} IBotCommand controllers", this.WithIBotCommand.Count());
+            Log.Debug("Found {quantity} IBotCommand methods", this.WithIBotCommand == null ? 0 : this.WithIBotCommand.SelectMany(x => x.Methods).Count());
         }
     }
 }
