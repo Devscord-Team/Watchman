dd8a026a83a8a8db09ae881ab864803232c9f637
Marcin Bigos <marcin.bigos99@gmail.com>
2020-07-15 17:07:35 +0200

    fix mention regex

--- Watchman.Discord/Areas/UselessFeatures/BotCommands/MarudaCommand.cs
+++ Watchman.Discord/Areas/UselessFeatures/BotCommands/MarudaCommand.cs
@@ -1,8 +1,13 @@
 ﻿using Devscord.DiscordFramework.Framework.Commands;
+using Devscord.DiscordFramework.Framework.Commands.PropertyAttributes;
 
 namespace Watchman.Discord.Areas.UselessFeatures.BotCommands
 {
     public class MarudaCommand : IBotCommand
     {
+        [UserMention]
+        public ulong User { get; set; }
+        [ChannelMention]
+        public ulong Channel { get; set; }
     }
 }
