9de112f76245788bc5c4997057603e6bcb25ae16
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-07-16 13:14:05 +0200

    add more safety checks while setting permissions for channels

--- Devscord.DiscordFramework/Services/ChannelsService.cs
+++ Devscord.DiscordFramework/Services/ChannelsService.cs
@@ -8,9 +8,9 @@ namespace Devscord.DiscordFramework.Services
 {
     public class ChannelsService
     {
-        public Task SetPermissions(ChannelContext channel, ChangedPermissions permissions, UserRole userRole)
+        public Task SetPermissions(ChannelContext channel, DiscordServerContext server, ChangedPermissions permissions, UserRole userRole)
         {
-            return Server.SetRolePermissions(channel, permissions, userRole);
+            return Server.SetRolePermissions(channel, server, permissions, userRole);
         }
 
         public Task SetPermissions(IEnumerable<ChannelContext> channels, DiscordServerContext server, ChangedPermissions permissions, UserRole userRole)
