b73ad294336ab86843d23ca47a836cd5fb0ba8f6
Marcin Bigos <marcin.bigos99@gmail.com>
2020-07-18 23:04:38 +0200

    empty IBotCommands and Prefixes Controller

---
+++ Watchman.Discord/Areas/Prefixes/Commands/PrefixesCommand.cs
@@ -0,0 +1,11 @@
+﻿using Devscord.DiscordFramework.Framework.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Watchman.Discord.Areas.Prefixes.Commands
+{
+    public class PrefixesCommand : IBotCommand
+    {
+    }
+}
---
+++ Watchman.Discord/Areas/Prefixes/PrefixesController.cs
@@ -0,0 +1,25 @@
+﻿using Devscord.DiscordFramework.Framework.Architecture.Controllers;
+using Devscord.DiscordFramework.Middlewares.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Watchman.Discord.Areas.Prefixes.Commands;
+
+namespace Watchman.Discord.Areas.Prefixes
+{
+    public class PrefixesController : IController
+    {
+        public async Task GetPrefixes(PrefixesCommand command, Contexts contexts)
+        {
+        }
+
+        public async Task AddPrefix(AddPrefixCommand command, Contexts contexts)
+        {
+        }
+
+        public async Task RemovePrefix(RemovePrefixCommand command, Contexts contexts)
+        {
+        }
+    }
+}
