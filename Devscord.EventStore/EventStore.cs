a9736c4218b6fb7e0482157cae988076e52dc135
Marcin Bigos <marcin.bigos99@gmail.com>
2020-07-19 03:41:51 +0200

    event store basic concept

---
+++ Devscord.EventStore/EventStore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Devscord.EventStore
+{
+    public static class EventStore //todo - save events
+    {
+        private static List<KeyValuePair<string, object>> _events = new List<KeyValuePair<string, object>>();
+        private static List<KeyValuePair<string, Action<object>>> _eventHandlers = new List<KeyValuePair<string, Action<object>>>();
+
+        public static void Publish<T>(T @event) where T : IEvent 
+        {
+
+        }
+
+        public static void Subscribe<T>(Action<T> action) where T : IEvent
+        {
+
+        }
+    }
+
+    public interface IEvent
+    {
+    }
+}
