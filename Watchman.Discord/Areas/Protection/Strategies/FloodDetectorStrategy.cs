7f1d545efb924a34ada298be542aba3aad8275fc
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-07-23 12:48:20 +0200

    add floodDetector

---
+++ Watchman.Discord/Areas/Protection/Strategies/FloodDetectorStrategy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using Devscord.DiscordFramework.Framework.Commands.AntiSpam;
+using Devscord.DiscordFramework.Framework.Commands.AntiSpam.Models;
+using Devscord.DiscordFramework.Framework.Commands.Parsing.Models;
+using Devscord.DiscordFramework.Middlewares.Contexts;
+using Watchman.DomainModel.Settings.ConfigurationItems;
+using Watchman.DomainModel.Settings.Services;
+
+namespace Watchman.Discord.Areas.Protection.Strategies
+{
+    public class FloodDetectorStrategy : ISpamDetector
+    {
+        private readonly IConfigurationService _configurationService;
+        public IUserSafetyChecker UserSafetyChecker { get; set; }
+
+        public FloodDetectorStrategy(IUserSafetyChecker userSafetyChecker, IConfigurationService configurationService)
+        {
+            this._configurationService = configurationService;
+            this.UserSafetyChecker = userSafetyChecker;
+        }
+
+        public SpamProbability GetSpamProbability(ServerMessagesCacheService serverMessagesCacheService, DiscordRequest request, Contexts contexts)
+        {
+            var howManyMessagesCount = this._configurationService.GetConfigurationItem<HowManyMessagesInShortTimeToBeSpam>(contexts.Server.Id).Value;
+            var howManySeconds = this._configurationService.GetConfigurationItem<HowLongIsShortTimeInSeconds>(contexts.Server.Id).Value;
+            var minDate = DateTime.UtcNow.AddSeconds(howManySeconds);
+            var messagesCount = serverMessagesCacheService.GetLastUserMessages(contexts.User.Id, contexts.Server.Id)
+                .TakeLast(howManyMessagesCount)
+                .Count(x => x.SentAt >= minDate);
+
+            if (messagesCount < howManyMessagesCount / 3)
+            {
+                return SpamProbability.None;
+            }
+            var userIsSafe = this.UserSafetyChecker.IsUserSafe(contexts.User.Id, contexts.Server.Id);
+            return userIsSafe switch
+            {
+                true when messagesCount < howManyMessagesCount => SpamProbability.None,
+                false when messagesCount < howManyMessagesCount / 2 => SpamProbability.Low,
+                false when messagesCount < howManyMessagesCount => SpamProbability.Medium,
+                _ => SpamProbability.Sure
+            };
+        }
+    }
+}
---
+++ Watchman.DomainModel/Settings/ConfigurationItems/HowManyMessagesInShortTimeToBeSpam.cs
@@ -0,0 +1,11 @@
+﻿namespace Watchman.DomainModel.Settings.ConfigurationItems
+{
+    public class HowManyMessagesInShortTimeToBeSpam : MappedConfiguration<int>
+    {
+        public override int Value { get; set; } = 15;
+
+        public HowManyMessagesInShortTimeToBeSpam(ulong serverId) : base(serverId)
+        {
+        }
+    }
+}
