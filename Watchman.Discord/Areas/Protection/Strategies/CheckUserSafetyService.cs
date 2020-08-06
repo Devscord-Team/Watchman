702237f6d6571dce27f60a004ae23f07eeeb1176
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-08-06 15:12:14 +0200

    small fix

--- Watchman.Discord/Areas/Protection/Strategies/CheckUserSafetyService.cs
+++ Watchman.Discord/Areas/Protection/Strategies/CheckUserSafetyService.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Autofac;
 using Devscord.DiscordFramework.Framework.Commands.AntiSpam;
 using Devscord.DiscordFramework.Services;
 using Serilog;
