b38348b219d928f14876985a3c94a8e7dbbcbf6c
Rafał Matyjaszyk <47067171+Rafal38@users.noreply.github.com>
2020-08-10 15:44:21 +0200

    Update Watchman.Discord/Areas/Users/Controllers/WarnsController.cs
    Co-authored-by: Mateusz Strycharz <strycharz.mateusz0@gmail.com>

--- Watchman.Discord/Areas/Users/Controllers/WarnsController.cs
+++ Watchman.Discord/Areas/Users/Controllers/WarnsController.cs
@@ -47,9 +47,7 @@ namespace Watchman.Discord.Areas.Protection.Controllers
         public async Task Warns(WarnsCommand command, Contexts contexts)
         {
             var messageService = _messagesServiceFactory.Create(contexts);
-            var mentionedUser = command.User == 0 ? 
-                    contexts.User : 
-                    await this._usersService.GetUserByIdAsync(contexts.Server, command.User);
+            var mentionedUser = command.User == 0 ? contexts.User : await this._usersService.GetUserByIdAsync(contexts.Server, command.User);
             if (mentionedUser == null)
             {
                 await messageService.SendResponse(x => x.UserNotFound(command.User.GetUserMention()));
