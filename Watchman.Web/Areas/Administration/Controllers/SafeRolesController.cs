c3f608167e6e1ef7a988b413ac708b57aa6ccaf4
Blanka Lewonowska <loczkovata@gmail.com>
2020-09-09 22:10:16 +0200

    implemented setting and removing safe roles

--- Watchman.Web/Areas/Administration/Controllers/SafeRolesController.cs
+++ Watchman.Web/Areas/Administration/Controllers/SafeRolesController.cs
@@ -1,18 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Enumeration;
 using System.Linq;
 using System.Threading.Tasks;
+using Hangfire.MemoryStorage.Entities;
+using Microsoft.AspNetCore.Mvc;
 using Watchman.Cqrs;
+using Watchman.DomainModel.DiscordServer.Commands;
 
 namespace Watchman.Web.Areas.Administration.Controllers
 {
     public class SafeRolesController : BaseApiController
     {
         private readonly IQueryBus _queryBus;
+        private readonly ICommandBus _commandBus;
 
-        public SafeRolesController(IQueryBus queryBus)
+        public SafeRolesController(IQueryBus queryBus, ICommandBus commandBus)
         {
             this._queryBus = queryBus;
+            this._commandBus = commandBus;
+        }
+
+        [HttpPost]
+        public async Task SetSafeRole(string roleName, ulong serverId = 0)
+        {
+            var command = new SetRoleAsSafeCommand(roleName,serverId);
+            await this._commandBus.ExecuteAsync(command);
+        }
+
+        [HttpPost]
+        public async Task RemoveSafeRole(string roleName, ulong serverId = 0)
+        {
+            var command = new SetRoleAsUnsafeCommand(roleName, serverId);
+            await this._commandBus.ExecuteAsync(command);
         }
     }
 }
