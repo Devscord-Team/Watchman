65c65953a2bf149677eda0cdd854c8c31f752a17
Blanka Lewonowska <loczkovata@gmail.com>
2020-09-09 22:09:48 +0200

    managing safe roles implemented

---
+++ Watchman.Web/Areas/Administration/Controllers/SafeRolesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Watchman.Cqrs;
+
+namespace Watchman.Web.Areas.Administration.Controllers
+{
+    public class SafeRolesController : BaseApiController
+    {
+        private readonly IQueryBus _queryBus;
+
+        public SafeRolesController(IQueryBus queryBus)
+        {
+            this._queryBus = queryBus;
+        }
+    }
+}
