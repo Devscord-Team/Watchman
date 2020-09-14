a772c7636eaf5ca3a4d600c85ddb5613432712d5
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-09-14 21:43:38 +0200

    fix compiling errors

--- Watchman.DomainModel/Configuration/ConfigurationItems/MaxNumberOfMessagesDisplayedByMessageCommandWithoutForce.cs
+++ Watchman.DomainModel/Configuration/ConfigurationItems/MaxNumberOfMessagesDisplayedByMessageCommandWithoutForce.cs
@@ -1,4 +1,4 @@
-﻿namespace Watchman.DomainModel.Settings.ConfigurationItems
+﻿namespace Watchman.DomainModel.Configuration.ConfigurationItems
 {
     public class MaxNumberOfMessagesDisplayedByMessageCommandWithoutForce : MappedConfiguration<int>
     {
