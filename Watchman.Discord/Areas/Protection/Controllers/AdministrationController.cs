1f28e23e9ceae9cb982eb3bdd83b05e0a442dfb0
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-09-14 21:22:07 +0200

    move to complaintsChannelService

--- Watchman.Discord/Areas/Protection/Controllers/AdministrationController.cs
+++ Watchman.Discord/Areas/Protection/Controllers/AdministrationController.cs
@@ -1,80 +1,31 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Devscord.DiscordFramework.Commons;
+﻿using System.Threading.Tasks;
 using Devscord.DiscordFramework.Framework.Architecture.Controllers;
 using Devscord.DiscordFramework.Framework.Commands.Responses;
 using Devscord.DiscordFramework.Middlewares.Contexts;
-using Devscord.DiscordFramework.Services;
 using Devscord.DiscordFramework.Services.Factories;
-using Watchman.Cqrs;
 using Watchman.Discord.Areas.Protection.BotCommands;
-using Watchman.DomainModel.Protection.Commands;
-using Watchman.DomainModel.Protection.Queries;
+using Watchman.Discord.Areas.Protection.Services;
 
 namespace Watchman.Discord.Areas.Protection.Controllers
 {
     public class AdministrationController : IController
     {
-        private readonly IQueryBus _queryBus;
-        private readonly ICommandBus _commandBus;
-        private readonly ChannelsService _channelsService;
-        private readonly UsersRolesService _usersRolesService;
         private readonly MessagesServiceFactory _messagesServiceFactory;
+        private readonly ComplaintsChannelService _complaintsChannelService;
 
-        public AdministrationController(IQueryBus queryBus, ICommandBus commandBus, ChannelsService channelsService, UsersRolesService usersRolesService, MessagesServiceFactory messagesServiceFactory)
+        public AdministrationController(MessagesServiceFactory messagesServiceFactory, ComplaintsChannelService complaintsChannelService)
         {
-            this._queryBus = queryBus;
-            this._commandBus = commandBus;
-            this._channelsService = channelsService;
-            this._usersRolesService = usersRolesService;
             this._messagesServiceFactory = messagesServiceFactory;
+            this._complaintsChannelService = complaintsChannelService;
         }
 
         [AdminCommand]
         public async Task CreateChannelForComplaints(ComplaintsChannelCommand command, Contexts contexts)
         {
-            // create channel
             var complaintsChannelName = string.IsNullOrWhiteSpace(command.Name) ? "skargi" : command.Name;
-
-            var query = new GetComplaintsChannelQuery(contexts.Server.Id);
-            var complaintsChannelId = this._queryBus.Execute(query).ComplaintsChannel.ChannelId;
-
+            _ = await this._complaintsChannelService.CreateComplaintsChannel(complaintsChannelName, contexts);
             var messagesService = this._messagesServiceFactory.Create(contexts);
-            if (complaintsChannelId == 0)
-            {
-                await messagesService.SendResponse(x => x.ComplaintsChannelAlreadyExists());
-                return;
-            }
-
-            var complaintsChannel = contexts.Server.GetTextChannels().FirstOrDefault(x => x.Id == complaintsChannelId)
-                ?? await this._channelsService.CreateNewChannelAsync(contexts.Server, complaintsChannelName);
-            // end create channel
-
-            //set perms
-            var readingAndSending = new List<Permission> { Permission.ReadMessages, Permission.SendMessages };
-
-            var everyonePermissions = new ChangedPermissions(allowPermissions: null, denyPermissions: readingAndSending);
-            var adminsPermissions = new ChangedPermissions(readingAndSending, null);
-            var mutedPermissions = new ChangedPermissions(readingAndSending, new List<Permission> { Permission.ReadMessageHistory });
-
-            var serverRoles = this._usersRolesService.GetRoles(contexts.Server).ToList();
-            var everyoneRole = serverRoles.First(x => x.Name == "@everyone");
-            var adminRoles = serverRoles.Where(x => x.Permissions.Contains(Permission.ManageGuild));
-            var mutedRole = serverRoles.FirstOrDefault(x => x.Name == UsersRolesService.MUTED_ROLE_NAME);
-
-            var setAdminPerms = adminRoles.Select(role => this._channelsService.SetPermissions(complaintsChannel, contexts.Server, adminsPermissions, role));
-            Task.WaitAll(setAdminPerms.ToArray());
-            await this._channelsService.SetPermissions(complaintsChannel, contexts.Server, mutedPermissions, mutedRole);
-            await this._channelsService.SetPermissions(complaintsChannel, contexts.Server, everyonePermissions, everyoneRole);
-            // end set perms
-
-            var addComplaintsChannelCommand = new AddComplaintsChannelCommand(complaintsChannel.Id);
-            await this._commandBus.ExecuteAsync(addComplaintsChannelCommand);
-
             await messagesService.SendResponse(x => x.ComplaintsChannelHasBeenCreated());
-
-            // assign action to ChannelRemoved to remove complaintsChannelId from the base, when the channel is deleted
         }
     }
 }
--- Watchman.Discord/Areas/Protection/Services/ComplaintsChannelService.cs
+++ Watchman.Discord/Areas/Protection/Services/ComplaintsChannelService.cs
@@ -1,9 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Devscord.DiscordFramework.Commons;
+using Devscord.DiscordFramework.Commons.Exceptions;
+using Devscord.DiscordFramework.Framework.Commands.Responses;
 using Devscord.DiscordFramework.Middlewares.Contexts;
+using Devscord.DiscordFramework.Services;
+using Devscord.DiscordFramework.Services.Factories;
 using Watchman.Cqrs;
+using Watchman.DomainModel.Protection.Commands;
 using Watchman.DomainModel.Protection.Queries;
 
 namespace Watchman.Discord.Areas.Protection.Services
@@ -12,17 +17,73 @@ namespace Watchman.Discord.Areas.Protection.Services
     {
         private readonly IQueryBus _queryBus;
         private readonly ICommandBus _commandBus;
+        private readonly MessagesServiceFactory _messagesServiceFactory;
+        private readonly ChannelsService _channelsService;
+        private readonly UsersRolesService _usersRolesService;
 
-        public ComplaintsChannelService(IQueryBus queryBus, ICommandBus commandBus)
+        public ComplaintsChannelService(IQueryBus queryBus, ICommandBus commandBus, MessagesServiceFactory messagesServiceFactory, ChannelsService channelsService, UsersRolesService usersRolesService)
         {
             this._queryBus = queryBus;
             this._commandBus = commandBus;
+            this._messagesServiceFactory = messagesServiceFactory;
+            this._channelsService = channelsService;
+            this._usersRolesService = usersRolesService;
         }
 
-        public async Task RemoveIfNeededComplaintsChannel(ChannelContext channel, DiscordServerContext server)
+        public Task RemoveIfNeededComplaintsChannel(ChannelContext channel, DiscordServerContext server)
         {
             var query = new GetComplaintsChannelQuery(server.Id);
             var complaintsChannel = this._queryBus.Execute(query).ComplaintsChannel;
+            if (channel.Id != complaintsChannel.ChannelId)
+            {
+                return Task.CompletedTask;
+            }
+            var command = new RemoveComplaintsChannelCommand(complaintsChannel);
+            return this._commandBus.ExecuteAsync(command);
+        }
+
+        public async Task<ChannelContext> CreateComplaintsChannel(string channelName, Contexts contexts)
+        {
+            if (this.IsComplaintsChannelAlreadyExisting(contexts.Server))
+            {
+                throw new ComplaintsChannelAlreadyExistsException();
+            }
+            var complaintsChannel = await this._channelsService.CreateNewChannelAsync(contexts.Server, channelName);
+            await this.SetChannelPermissions(complaintsChannel, contexts);
+            await this.NotifyDomainAboutComplaintsChannel(complaintsChannel, contexts.Server);
+            return complaintsChannel;
+        }
+
+        private bool IsComplaintsChannelAlreadyExisting(DiscordServerContext server)
+        {
+            var query = new GetComplaintsChannelQuery(server.Id);
+            var complaintsChannelId = this._queryBus.Execute(query).ComplaintsChannel.ChannelId;
+            return complaintsChannelId != 0;
+        }
+
+        private async Task SetChannelPermissions(ChannelContext channel, Contexts contexts)
+        {
+            var readingAndSending = new List<Permission> { Permission.ReadMessages, Permission.SendMessages };
+
+            var everyonePermissions = new ChangedPermissions(allowPermissions: null, denyPermissions: readingAndSending);
+            var adminsPermissions = new ChangedPermissions(readingAndSending, null);
+            var mutedPermissions = new ChangedPermissions(readingAndSending, new List<Permission> { Permission.ReadMessageHistory });
+
+            var serverRoles = this._usersRolesService.GetRoles(contexts.Server).ToList();
+            var everyoneRole = serverRoles.First(x => x.Name == "@everyone");
+            var adminRoles = serverRoles.Where(x => x.Permissions.Contains(Permission.ManageGuild));
+            var mutedRole = serverRoles.FirstOrDefault(x => x.Name == UsersRolesService.MUTED_ROLE_NAME);
+
+            var setAdminPerms = adminRoles.Select(role => this._channelsService.SetPermissions(channel, contexts.Server, adminsPermissions, role));
+            Task.WaitAll(setAdminPerms.ToArray());
+            await this._channelsService.SetPermissions(channel, contexts.Server, mutedPermissions, mutedRole);
+            await this._channelsService.SetPermissions(channel, contexts.Server, everyonePermissions, everyoneRole);
+        }
+
+        private Task NotifyDomainAboutComplaintsChannel(ChannelContext channel, DiscordServerContext server)
+        {
+            var addComplaintsChannelCommand = new AddComplaintsChannelCommand(channel.Id, server.Id);
+            return this._commandBus.ExecuteAsync(addComplaintsChannelCommand);
         }
     }
 }
