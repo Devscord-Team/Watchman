9b585cb96cfa5530d4d3ad81121e58fec6083a55
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-10-10 21:19:36 +0200

    remove useless code

--- Watchman.DomainModel/Configuration/Services/ConfigurationService.cs
+++ Watchman.DomainModel/Configuration/Services/ConfigurationService.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Autofac;
-using Watchman.DomainModel.Configuration.ConfigurationChangesHandlers;
-using Watchman.DomainModel.Configuration.ConfigurationItems;
 using Watchman.Integrations.MongoDB;
 
 namespace Watchman.DomainModel.Configuration.Services
@@ -17,7 +15,6 @@ namespace Watchman.DomainModel.Configuration.Services
         private readonly ConfigurationMapperService _configurationMapperService;
         private readonly ConfigurationItemsSearcherService _configurationTypesSearcher;
         private static Dictionary<Guid, int> _configurationVersions;
-        private readonly List<(string configurationName, ulong serverId, Func<IMappedConfiguration, Task> func)> _afterConfigurationChanged = new List<(string, ulong, Func<IMappedConfiguration, Task>)>();
         private static Dictionary<Type, Dictionary<ulong, IMappedConfiguration>> _cachedConfigurationItem;
 
         public ConfigurationService(IComponentContext componentContext, ISessionFactory sessionFactory, ConfigurationMapperService configurationMapperService, ConfigurationItemsSearcherService configurationTypesSearcher)
@@ -34,11 +31,6 @@ namespace Watchman.DomainModel.Configuration.Services
             var configurations = _cachedConfigurationItem[typeof(T)];
             var serverConfiguration = configurations.GetValueOrDefault(serverId) ?? configurations[DEFAULT_SERVER_ID];
             return (T)serverConfiguration;
-        }
-
-        public void AddOnConfigurationChanged(string configurationName, ulong serverId, Func<IMappedConfiguration, Task> func)
-        {
-            this._afterConfigurationChanged.Add((configurationName, serverId, func));
         }
 
         public IEnumerable<IMappedConfiguration> GetConfigurationItems(ulong serverId)
@@ -105,25 +97,7 @@ namespace Watchman.DomainModel.Configuration.Services
                 Task.WaitAll(tasks.ToArray());
             }
             _configurationVersions = configurationItems.ToDictionary(x => x.Id, x => x.Version);
-            //var changedConfigurations = mappedConfigurations.SelectMany(x =>
-            //{
-            //    var (configurationType, serversConfigurations) = x;
-            //    return serversConfigurations.Where(configuration =>
-            //    {
-            //        var (serverId, mappedConfiguration) = configuration;
-            //        var existingConfiguration = this._cachedConfigurationItem[configurationType].GetValueOrDefault(serverId);
-            //        return !mappedConfiguration.Equals(existingConfiguration);
-            //    });
-            //});
-            //var afterConfigurationChangedTasks = new List<Task>();
-            //foreach (var (serverId, configuration) in changedConfigurations)
-            //{
-            //    var funcsToCall = this._afterConfigurationChanged.Where(x => x.serverId == serverId && x.configurationName == configuration.Name);
-            //    var tasks = funcsToCall.Select(x => x.func!.Invoke(configuration));
-            //    afterConfigurationChangedTasks.AddRange(tasks);
-            //}
             _cachedConfigurationItem = mappedConfigurations;
-            //Task.WaitAll(afterConfigurationChangedTasks.ToArray());
         }
 
         private IConfigurationChangesHandler<IMappedConfiguration> GetConfigurationChangesHandler(IMappedConfiguration newMappedConfiguration)
--- Watchman.DomainModel/Configuration/Services/IConfigurationService.cs
+++ Watchman.DomainModel/Configuration/Services/IConfigurationService.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Watchman.DomainModel.Configuration.Services
@@ -7,7 +6,6 @@ namespace Watchman.DomainModel.Configuration.Services
     public interface IConfigurationService
     {
         T GetConfigurationItem<T>(ulong serverId) where T : IMappedConfiguration;
-        void AddOnConfigurationChanged(string configurationName, ulong serverId, Func<IMappedConfiguration, Task> func);
         IEnumerable<IMappedConfiguration> GetConfigurationItems(ulong serverId);
         Task SaveNewConfiguration(IMappedConfiguration changedConfiguration);
     }
