b6181fca5371044ec8cd1d03d50f679133597e1b
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-10-17 17:44:53 +0200

    cleaning

--- Watchman.Discord/Areas/Responses/BotCommands/ResponsesCommand.cs
+++ Watchman.Discord/Areas/Responses/BotCommands/ResponsesCommand.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Devscord.DiscordFramework.Framework.Commands;
+﻿using Devscord.DiscordFramework.Framework.Commands;
 using Devscord.DiscordFramework.Framework.Commands.PropertyAttributes;
-using Devscord.DiscordFramework.Framework.Commands.Parsing.Models;
 
 namespace Watchman.Discord.Areas.Responses.BotCommands
 {
