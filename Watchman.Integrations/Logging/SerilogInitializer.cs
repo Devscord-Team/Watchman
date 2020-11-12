bc7c7f45bdaf7462a655b262d400b331456d4821
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-11-12 23:54:41 +0100

    fix logging

--- Watchman.Integrations/Logging/SerilogInitializer.cs
+++ Watchman.Integrations/Logging/SerilogInitializer.cs
@@ -14,15 +14,16 @@ namespace Watchman.Integrations.Logging
 #if DEBUG
             Serilog.Debugging.SelfLog.Enable(msg => Debug.WriteLine(msg));
 #endif
+            var jsonFormatter = new JsonFormatter(closingDelimiter: $",{Environment.NewLine}");
             var logger = new LoggerConfiguration()
                 .MinimumLevel.Verbose()
                 .Enrich.FromLogContext()
                 .Enrich.WithThreadId()
                 .Enrich.WithMachineName()
                 .Enrich.WithEnvironmentUserName()
-                .WriteTo.File(new JsonFormatter(), "logs/all/log-{Date}.json", rollingInterval: RollingInterval.Day, shared: true, flushToDiskInterval: TimeSpan.FromSeconds(15), restrictedToMinimumLevel: LogEventLevel.Verbose)
-                .WriteTo.File(new JsonFormatter(closingDelimiter: ","), "logs/warningplus/log-{Date}.json", rollingInterval: RollingInterval.Day, shared: true, flushToDiskInterval: TimeSpan.FromSeconds(15), restrictedToMinimumLevel: LogEventLevel.Warning)
-                .WriteTo.File(new JsonFormatter(closingDelimiter: ","), "logs/errorplus/log-{Date}.json", rollingInterval: RollingInterval.Day, shared: true, flushToDiskInterval: TimeSpan.FromSeconds(15), restrictedToMinimumLevel: LogEventLevel.Error)
+                .WriteTo.File(jsonFormatter, "logs/all/log-.json", rollingInterval: RollingInterval.Day, shared: true, flushToDiskInterval: TimeSpan.FromSeconds(15), restrictedToMinimumLevel: LogEventLevel.Verbose)
+                .WriteTo.File(jsonFormatter, "logs/warningplus/log-.json", rollingInterval: RollingInterval.Day, shared: true, flushToDiskInterval: TimeSpan.FromSeconds(15), restrictedToMinimumLevel: LogEventLevel.Warning)
+                .WriteTo.File(jsonFormatter, "logs/errorplus/log-.json", rollingInterval: RollingInterval.Day, shared: true, flushToDiskInterval: TimeSpan.FromSeconds(15), restrictedToMinimumLevel: LogEventLevel.Error)
                 .WriteTo.Console(
                     restrictedToMinimumLevel: LogEventLevel.Verbose,
                     outputTemplate: "[{Timestamp:dd-MM-yyyy} - {Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
