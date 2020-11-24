d845db38d8f62612dfeb54686cd868282d6d1ffd
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-11-24 22:55:18 +0100

    add command

---
+++ Watchman.Discord/Areas/UselessFeatures/BotCommands/DontaskCommand.cs
@@ -0,0 +1,8 @@
+﻿using Devscord.DiscordFramework.Framework.Commands;
+
+namespace Watchman.Discord.Areas.UselessFeatures.BotCommands
+{
+    public class DontaskCommand : IBotCommand
+    {
+    }
+}
