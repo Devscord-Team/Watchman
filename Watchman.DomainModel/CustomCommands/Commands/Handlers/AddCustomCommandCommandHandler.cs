6fa4f7f9f18c78e29299feb8347a38198fedc520
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-12-02 11:05:05 +0100

    fix commands regex templates

---
+++ Watchman.DomainModel/CustomCommands/Commands/Handlers/AddCustomCommandCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Watchman.Cqrs;
+using Watchman.Integrations.Database;
+
+namespace Watchman.DomainModel.CustomCommands.Commands.Handlers
+{
+    public class AddCustomCommandCommandHandler : ICommandHandler<AddCustomCommandsCommand>
+    {
+        private readonly ISessionFactory _sessionFactory;
+
+        public AddCustomCommandCommandHandler(ISessionFactory sessionFactory)
+        {
+            this._sessionFactory = sessionFactory;
+        }
+
+        public async Task HandleAsync(AddCustomCommandsCommand command)
+        {
+            using var session = this._sessionFactory.CreateMongo();
+            var templateRegex = command.CustomTemplateRegex.StartsWith('^')
+                ? command.CustomTemplateRegex
+                : $"^{command.CustomTemplateRegex}";
+            var customCommand = new CustomCommand(command.CommandFullName, templateRegex, command.ServerId);
+            await session.AddAsync(customCommand);
+        }
+    }
+}
