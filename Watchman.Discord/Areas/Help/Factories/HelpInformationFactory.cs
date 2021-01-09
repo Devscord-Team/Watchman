cace40d62bac41b044e90566f9bb1ac12dd86874
Patryk Wiśniewski <wisniewskii.patryk@o2.pl>
2021-01-09 22:05:57 +0100

    Add description to all available commands

--- Watchman.Discord/Areas/Help/Factories/HelpInformationFactory.cs
+++ Watchman.Discord/Areas/Help/Factories/HelpInformationFactory.cs
@@ -17,7 +17,13 @@ namespace Watchman.Discord.Areas.Help.Factories
         public HelpInformation Create(BotCommandInformation commandInformation)
         {
             var argumentInformations = commandInformation.BotCommandArgumentInformations.Select(x => this._argumentInfoFactory.Create(x));
-            var descriptions = new List<Description> { new Description() };
+            var desciptionCommand = typeof(Devscord.DiscordFramework.Framework.Commands.BotCommandDescription.CommandDescription).GetProperties()
+                .Where(x => x.PropertyType.Name == "String" && x.Name == commandInformation.Name)
+                .Select(prop =>
+                { 
+                    return prop.GetValue(prop)?.ToString();
+                }).FirstOrDefault();
+            var descriptions = string.IsNullOrEmpty(desciptionCommand) ? new List<Description>() : new List<Description> { new Description() { Language = "EN", Text = desciptionCommand } };
             return new HelpInformation(commandInformation.Name, commandInformation.AreaName, argumentInformations, descriptions, HelpInformation.EMPTY_SERVER_ID);
         }
     }
