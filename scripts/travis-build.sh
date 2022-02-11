01dda5e2202f6d45566a156dab2e1491e8a2b3c0
Marcin Bigos <marcin.bigos99@gmail.com>
2022-02-11 19:34:10 +0100

    Update travis-build.sh

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
 sudo snap alias dotnet-sdk.dotnet dotnet
+
 dotnet build --configuration Release
+
 dotnet test
+
+curl -Os https://uploader.codecov.io/latest/linux/codecov
+chmod +x codecov
+./codecov
