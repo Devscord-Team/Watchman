6f955329f389e17e56562404362784093fa3590d
Marcin Bigos <marcin.bigos99@gmail.com>
2022-02-11 20:57:48 +0100

    remove files list from codecov

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -9,9 +9,4 @@ dotnet test Watchman.sln /p:CollectCoverage=true /p:CoverletOutputFormat=opencov
 curl -Os https://uploader.codecov.io/latest/linux/codecov
 chmod +x codecov
 
-./codecov -f "/Devscord.DiscordFramework.UnitTests/coverage.opencover.xml"
-./codecov -f "/ProjectStructureTests/coverage.opencover.xml"
-./codecov -f "/Statsman.Tests/coverage.opencover.xml"
-./codecov -f "/Watchman.Discord.UnitTests/coverage.opencover.xml"
-./codecov -f "/Watchman.DomainModel.UnitTests/coverage.opencover.xml"
-./codecov -f "/Watchman.Integration.Tests/coverage.opencover.xml"
+./codecov
