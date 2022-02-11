2a12f4e930373e8d8c7174c940f4277a81c99398
Marcin Bigos <marcin.bigos99@gmail.com>
2022-02-11 22:35:05 +0100

    Update travis-build.sh

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -9,4 +9,7 @@ dotnet test Watchman.sln /p:CollectCoverage=true /p:CoverletOutputFormat=opencov
 curl -Os https://uploader.codecov.io/latest/linux/codecov
 chmod +x codecov
 
-./codecov
+./codecov -f "/Devscord.DiscordFramework.UnitTests/coverage.opencover.xml"
+./codecov -f "/Watchman.Discord.UnitTests/coverage.opencover.xml"
+./codecov -f "/Watchman.DomainModel.UnitTests/coverage.opencover.xml"
+./codecov -f "/Watchman.Integration.Tests/coverage.opencover.xml"
