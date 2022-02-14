aa4a889e6befe55090b3138de3c17811386ee593
Tramp <Trampus749@gmail.com>
2022-02-14 22:32:00 +0100

    Adding Assertion to Test, also working on making it work.

--- Watchman.Discord.UnitTests/Warns/AddWarnTests.cs
+++ Watchman.Discord.UnitTests/Warns/AddWarnTests.cs
@@ -14,23 +14,54 @@ using System.Threading.Tasks;
 using Watchman.Discord.Areas.Protection.Services;
 using Watchman.Discord.Areas.Users.BotCommands;
 using FluentAssertions;
+using Watchman.Discord.UnitTests.TestObjectFactories;
+using Devscord.DiscordFramework.Commands.AntiSpam.Models;
 
 namespace Watchman.Discord.UnitTests.Warns
 {
 
     internal class AddWarnTests
     {
+        private readonly TestControllersFactory testControllersFactory = new();
+        private readonly TestContextsFactory testContextsFactory = new();
+
 
 
 
         [Test, AutoData]
         public async Task ShouldAddWarnToUser(AddWarnCommand command, Contexts contexts)
         {
-            var messageServiceFactoryMock = new Mock<IMessagesServiceFactory>();
-            var warnServiceMock = new Mock<IWarnsService>();
-            var usersServiceMock = new Mock<IUsersService>();
+            //Arrange
+            var userContext = testContextsFactory.CreateUserContext(1);
+            var userBotContext = testContextsFactory.CreateUserContext(2);
 
             
+
+            var messagesServiceFactoryMock = new Mock<IMessagesServiceFactory>();
+            var warnsServiceMock = new Mock<IWarnsService>();
+            var usersServiceMock = new Mock<IUsersService>();
+
+            usersServiceMock.Setup(x => x.GetUserByIdAsync(It.IsAny<DiscordServerContext>(), It.IsAny<ulong>()))
+                .Returns<DiscordServerContext, ulong>((a, b) => Task.FromResult(userBotContext));
+            usersServiceMock.Setup(x => x.GetBot())
+                .Returns(userBotContext);
+            contexts.SetContext(userContext);
+
+
+            var controller = this.testControllersFactory.CreateWarnsController(
+                messagesServiceFactoryMock: messagesServiceFactoryMock,
+                warnsServiceMock: warnsServiceMock,
+                usersServiceMock: usersServiceMock);
+
+            //Asset
+              await controller.AddWarn(command, contexts);
+
+            //Assert
+            messagesServiceFactoryMock.Verify(x => x.Create(contexts), Times.Once);
+            warnsServiceMock.Verify(x => x.AddWarnToUser(command, contexts, userContext), Times.Once);
+
+
+
 
             //var messageService = messageServiceFactoryMock.Create(contexts);
             //var mentionedUser = await this._usersService.GetUserByIdAsync(contexts.Server, command.User);
