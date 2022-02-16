fc0e7fa3df5993823c7ed4544cadcbba4f3daca9
Tramp <Trampus749@gmail.com>
2022-02-16 02:43:07 +0100

    Implemented GetWarns Tests

--- Watchman.Discord.UnitTests/Warns/WarnsControllerTests.cs
+++ Watchman.Discord.UnitTests/Warns/WarnsControllerTests.cs
@@ -16,6 +16,8 @@ using Watchman.Discord.Areas.Users.BotCommands;
 using FluentAssertions;
 using Watchman.Discord.UnitTests.TestObjectFactories;
 using Devscord.DiscordFramework.Commands.AntiSpam.Models;
+using Watchman.Discord.Areas.Users.BotCommands.Warns;
+using Devscord.DiscordFramework.Commons.Exceptions;
 
 namespace Watchman.Discord.UnitTests.Warns
 {
@@ -45,13 +47,88 @@ namespace Watchman.Discord.UnitTests.Warns
                 warnsServiceMock: warnsServiceMock,
                 usersServiceMock: usersServiceMock);
             
-            //Asset
+            //Act
             await controller.AddWarn(command, contexts);
 
             //Assert
             messagesServiceFactoryMock.Verify(x => x.Create(contexts), Times.Once);
             warnsServiceMock.Verify(x => x.AddWarnToUser(command, contexts, userContext), Times.Once);
             usersServiceMock.Verify(x => x.GetUserByIdAsync(contexts.Server, command.User), Times.Once);
+        }
+
+        [Test]
+        public async Task GetWarn_ShouldReturnWarnsOfCurrentUserIfNotMentioned()
+        {
+            //Arrange
+            var contexts = testContextsFactory.CreateContexts(1, 1, 1);
+            var command = new WarnsCommand();
+
+            var messagesServiceMock = new Mock<IMessagesService>();
+            var messagesServiceFactoryMock = new Mock<IMessagesServiceFactory>();
+            messagesServiceFactoryMock.Setup(x => x.Create(It.IsAny<Contexts>()))
+                .Returns(messagesServiceMock.Object);
+            var warnsServiceMock = new Mock<IWarnsService>();
+            warnsServiceMock.Setup(x => x.GetWarns(It.IsAny<UserContext>(), It.IsAny<ulong>()))
+                .Returns<UserContext, ulong>((a, b) => new List<KeyValuePair<string, string>>());
+
+            var controller = this.testControllersFactory.CreateWarnsController(
+                messagesServiceFactoryMock: messagesServiceFactoryMock,
+                warnsServiceMock: warnsServiceMock);
+
+            //Act
+            await controller.GetWarns(command, contexts);
+
+            //Assert
+            messagesServiceFactoryMock.Verify(x => x.Create(contexts), Times.Once);
+            warnsServiceMock.Verify(x => x.GetWarns(contexts.User, contexts.Server.Id), Times.Once);
+            messagesServiceMock.Verify(x => x.SendEmbedMessage(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<IEnumerable<KeyValuePair<string, string>>>()), Times.Once);
+
+        }
+
+        [Test]
+        [TestCase(5u)]
+        public async Task GetWarn_ShouldReturnWarnsOfMentionedUser(ulong mentionedUserId)
+        {
+            //Arrange
+            var mentionedUserContext = testContextsFactory.CreateUserContext(mentionedUserId);
+            var contexts = testContextsFactory.CreateContexts(1, 1, 1);
+            var command = new WarnsCommand() { User = mentionedUserId };
+
+            var messagesServiceMock = new Mock<IMessagesService>();
+            var messagesServiceFactoryMock = new Mock<IMessagesServiceFactory>();
+            messagesServiceFactoryMock.Setup(x => x.Create(It.IsAny<Contexts>()))
+                .Returns(messagesServiceMock.Object);
+            var warnsServiceMock = new Mock<IWarnsService>();
+            warnsServiceMock.Setup(x => x.GetWarns(It.IsAny<UserContext>(), It.IsAny<ulong>()))
+                .Returns<UserContext, ulong>((a, b) => new List<KeyValuePair<string, string>>());
+            var usersServiceMock = new Mock<IUsersService>();
+            usersServiceMock.Setup(x => x.GetUserByIdAsync(It.IsAny<DiscordServerContext>(), It.IsAny<ulong>()))
+                .Returns(Task.FromResult(mentionedUserContext));
+            var controller = this.testControllersFactory.CreateWarnsController(
+                messagesServiceFactoryMock: messagesServiceFactoryMock,
+                warnsServiceMock: warnsServiceMock,
+                usersServiceMock: usersServiceMock);
+
+            //Act
+            await controller.GetWarns(command, contexts);
+
+            //Assert
+            messagesServiceFactoryMock.Verify(x => x.Create(contexts), Times.Once);
+            usersServiceMock.Verify(x => x.GetUserByIdAsync(contexts.Server, mentionedUserId));
+            warnsServiceMock.Verify(x => x.GetWarns(mentionedUserContext, contexts.Server.Id), Times.Once);
+            messagesServiceMock.Verify(x => x.SendEmbedMessage(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<IEnumerable<KeyValuePair<string, string>>>()), Times.Once);
+        }
+
+        [Test]
+        public async Task GetWarn_ShouldThrowExceptionIfUserNotFound()
+        {
+            //Arrange
+            var contexts = testContextsFactory.CreateContexts(1, 1, 1);
+            var command = new WarnsCommand() { User = 2 };
+
+            var controller = this.testControllersFactory.CreateWarnsController();
+            //Act
+            Assert.ThrowsAsync<UserNotFoundException>(() => controller.GetWarns(command, contexts));
         }
     }
 }
