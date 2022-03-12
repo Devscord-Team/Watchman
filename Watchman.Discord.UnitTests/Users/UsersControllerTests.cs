d8415c939adb45c5ac510f0331c73275f08d2340
Tramp <Trampus749@gmail.com>
2022-03-12 15:12:09 +0100

    added some changes

--- Watchman.Discord.UnitTests/Users/UsersControllerTests.cs
+++ Watchman.Discord.UnitTests/Users/UsersControllerTests.cs
@@ -1,5 +1,7 @@
-﻿using Devscord.DiscordFramework.Middlewares.Contexts;
+﻿using AutoFixture.NUnit3;
+using Devscord.DiscordFramework.Middlewares.Contexts;
 using Devscord.DiscordFramework.Services;
+using Devscord.DiscordFramework.Services.Factories;
 using Moq;
 using NUnit.Framework;
 using System;
@@ -17,7 +19,7 @@ namespace Watchman.Discord.UnitTests.Users
         private readonly TestControllersFactory testControllersFactory = new();
         private readonly TestContextsFactory testContextsFactory = new();
 
-        [Test]
+        [Test, AutoData]
         public async Task GetAvatar_ShouldDisplayUserAvatar(AvatarCommand command)
         {
             //Arrange
@@ -25,6 +27,8 @@ namespace Watchman.Discord.UnitTests.Users
             var contexts = testContextsFactory.CreateContexts(1, 1, 1);
 
             var messagesServiceMock = new Mock<IMessagesService>();
+            var messagesServiceFactoryMock = new Mock<IMessagesServiceFactory>();
+            messagesServiceFactoryMock.Setup(x => x.Create(It.IsAny))
             var usersServiceMock = new Mock<IUsersService>();
             usersServiceMock.Setup(x => x.GetUserByIdAsync(It.IsAny<DiscordServerContext>(), It.IsAny<ulong>()))
                 .Returns<DiscordServerContext, ulong>((a, b) => Task.FromResult(userContext));
