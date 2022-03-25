3feb41243ae3fddb551175d7e80ca4ac6920d1bf
Tramp <Trampus749@gmail.com>
2022-03-25 01:37:47 +0100

    próbka

--- Watchman.Discord.UnitTests/Responses/ResponsesControllerTests.cs
+++ Watchman.Discord.UnitTests/Responses/ResponsesControllerTests.cs
@@ -21,13 +21,13 @@ namespace Watchman.Discord.UnitTests.Responses
     {
         private readonly TestControllersFactory testControllersFactory = new();
         private readonly TestContextsFactory testContextsFactory = new();
+
         [Test, AutoData]
         public async Task UpdateResponse_ShouldUpdateResponse(UpdateResponseCommand command)
         {
             //Arrange
-            var expectedResponse = new DomainModel.Responses.Response("test", "test", (ulong)43, new string[] { "test" });
-            var serverId = 5u;
-            var contexts = this.testContextsFactory.CreateContexts(serverId, 1, 1);
+            var expectedResponse = new DomainModel.Responses.Response("test", "test", 43ul, new string[] { "test" });
+            var contexts = this.testContextsFactory.CreateContexts(5, 1, 1);
             var messagesServiceMock = new Mock<IMessagesService>();
             var messagesServiceFactoryMock = new Mock<IMessagesServiceFactory>();
             messagesServiceFactoryMock.Setup(x => x.Create(It.IsAny<Contexts>()))
@@ -45,9 +45,9 @@ namespace Watchman.Discord.UnitTests.Responses
 
             //Assert
             messagesServiceFactoryMock.Verify(x => x.Create(contexts), Times.Once);
-            responsesServiceMock.Verify(x => x.GetResponseByOnEvent(It.IsAny<string>(), serverId), Times.Once);
+            responsesServiceMock.Verify(x => x.GetResponseByOnEvent(It.IsAny<string>(), contexts.Server.Id), Times.Once);
             responsesServiceMock.Verify(x => x.GetResponseByOnEvent(It.IsAny<string>(), DomainModel.Responses.Response.DEFAULT_SERVER_ID), Times.Once);
-            responsesServiceMock.Verify(x => x.RemoveResponse(It.IsAny<string>(), It.Is<ulong>(a => a != contexts.Server.Id)), Times.Never);
+            responsesServiceMock.Verify(x => x.RemoveResponse(It.IsAny<string>(), It.IsAny<ulong>()), Times.Never);
             responsesServiceMock.Verify(x => x.UpdateResponse(expectedResponse.Id, It.IsAny<string>()), Times.Once);
         }
 
@@ -73,6 +73,8 @@ namespace Watchman.Discord.UnitTests.Responses
             //Assert
             messagesServiceFactoryMock.Verify(x => x.Create(contexts), Times.Once);
             responsesServiceMock.Verify(x => x.RemoveResponse(It.IsAny<string>(), It.IsAny<ulong>()), Times.Never);
+            responsesServiceMock.Verify(x => x.UpdateResponse(It.IsAny<Guid>(), It.IsAny<string>()), Times.Never);
+            messagesServiceMock.Verify(x => x.SendResponse(It.IsAny<Func<Devscord.DiscordFramework.Commands.Responses.IResponsesService, string>>()), Times.Once);
         }
 
         [Test, AutoData]
@@ -80,7 +82,7 @@ namespace Watchman.Discord.UnitTests.Responses
         {
             //Arrange
             var contexts = this.testContextsFactory.CreateContexts(5, 1, 1);
-            var expectedResponse = new DomainModel.Responses.Response("test", "test", (ulong)43, new string[] { "test" });
+            var expectedResponse = new DomainModel.Responses.Response("test", "test", 43ul, new string[] { "test" });
 
             var messagesServiceMock = new Mock<IMessagesService>();
             var messagesServiceFactoryMock = new Mock<IMessagesServiceFactory>();
@@ -88,7 +90,7 @@ namespace Watchman.Discord.UnitTests.Responses
                 .Returns(messagesServiceMock.Object);
             var responsesServiceMock = new Mock<IResponsesService>();
             responsesServiceMock.Setup(x => x.GetResponseByOnEvent(It.IsAny<string>(), It.IsAny<ulong>()))
-               .Returns(Task.FromResult(new DomainModel.Responses.Response("test", command.Message, 43u, new string[] { "test" })));
+               .Returns(Task.FromResult(expectedResponse));
             
             var controller = this.testControllersFactory.CreateResponsesController(
                 messagesServiceFactoryMock: messagesServiceFactoryMock,
@@ -101,7 +103,7 @@ namespace Watchman.Discord.UnitTests.Responses
             messagesServiceFactoryMock.Verify(x => x.Create(contexts), Times.Once);
             responsesServiceMock.Verify(x => x.GetResponseByOnEvent(It.IsAny<string>(), DomainModel.Responses.Response.DEFAULT_SERVER_ID), Times.Once);
             responsesServiceMock.Verify(x => x.RemoveResponse(It.IsAny<string>(), It.IsAny<ulong>()), Times.Once);
-            responsesServiceMock.Verify(x => x.UpdateResponse(expectedResponse.Id, It.IsAny<string>()), Times.Never);
+            responsesServiceMock.Verify(x => x.UpdateResponse(It.IsAny<Guid>(), It.IsAny<string>()), Times.Never);
         }
     }
 }
