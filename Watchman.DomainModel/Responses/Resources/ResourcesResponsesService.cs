1053284d121a7f682b7376a3794e38d08999e306
Marcin Bigos <marcin.bigos99@gmail.com>
2022-04-02 00:32:04 +0200

    separate classes

--- Watchman.DomainModel/Responses/Resources/ResourcesResponsesService.cs
+++ Watchman.DomainModel/Responses/Resources/ResourcesResponsesService.cs
@@ -43,12 +43,4 @@ namespace Watchman.DomainModel.Responses.Areas.Administration
             }
         }
     }
-
-    public class DefaultResponseModel
-    {
-        public string OnEvent { get; set; }
-        public string Value { get; set; }
-        public string[] Variables { get; set; }
-        public bool RequireContext { get; set; }
-    }
 }
