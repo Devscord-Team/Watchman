59872f8028497c97b7fb7b87335fcd63cfb807f2
Marcin Bigos <marcin.bigos99@gmail.com>
2022-04-05 18:59:43 +0200

    get configurations command

--- Watchman.Discord/Areas/Configurations/Controllers/ConfigurationsController.cs
+++ Watchman.Discord/Areas/Configurations/Controllers/ConfigurationsController.cs
@@ -8,29 +8,36 @@ using System.Text;
 using System.Threading.Tasks;
 using Watchman.Cqrs;
 using Watchman.Discord.Areas.Configurations.IBotCommands;
+using Watchman.DomainModel.Configuration;
 using Watchman.DomainModel.Configuration.Queries;
+using Watchman.DomainModel.Configuration.Services;
 
 namespace Watchman.Discord.Areas.Configurations.Controllers
 {
     public class ConfigurationsController : IController
     {
-        private readonly IQueryBus queryBus;
         private readonly IMessagesServiceFactory messagesServiceFactory;
+        private readonly IConfigurationService configurationService;
 
-        public ConfigurationsController(IQueryBus queryBus, IMessagesServiceFactory messagesServiceFactory)
+        public ConfigurationsController(IQueryBus queryBus, IMessagesServiceFactory messagesServiceFactory, IConfigurationService configurationService)
         {
-            this.queryBus = queryBus;
             this.messagesServiceFactory = messagesServiceFactory;
+            this.configurationService = configurationService;
         }
 
+        //todo tests
         public async Task GetConfigurations(ConfigurationsCommand command, Contexts contexts)
         {
             var getConfigurationsQuery = new GetConfigurationItemsQuery(command.Group);
-            var configurationItems = (await this.queryBus.ExecuteAsync(getConfigurationsQuery)).ConfigurationItems;
-            var messagesService = this.messagesServiceFactory.Create(contexts);
-            var groupped = configurationItems.GroupBy(x => x.Group).OrderBy(x => x.Key);
-            var mapped = groupped.Select(x => new KeyValuePair<string, string>(x.Key, $"```\n{string.Join("\n", x.Select(item => $"{item.Name} ({item.Value.GetType().Name})"))}```"));
+            var configurations = this.configurationService.GetConfigurationItems(contexts.Server.Id);
+            if(command.Group != null)
+            {
+                configurations = configurations.Where(x => x.Group.ToLower() == command.Group.ToLower());
+            }
+            var groupped = configurations.GroupBy(x => x.Group).OrderBy(x => x.Key);
+            var mapped = groupped.Select(x => new KeyValuePair<string, string>(x.Key, $"```\n{string.Join("\n", x.Select(item => item.Name))}```"));
             //todo from responses
+            var messagesService = this.messagesServiceFactory.Create(contexts);
             await messagesService.SendEmbedMessage("Konfiguracja", "Poniżej znajdziesz liste elementów konfiguracji", mapped);
         }
     }
