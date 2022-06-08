45e74dbd99ab44b91204aad33dea271e5aea8a5a
DavidPL-coder <dawid2002@hotmail.com>
2022-06-08 19:55:46 +0200

    Add data migrations system for MongoDB database.

--- Watchman.Integrations/Database/MongoDB/MongoConfiguration.cs
+++ Watchman.Integrations/Database/MongoDB/MongoConfiguration.cs
@@ -1,7 +1,12 @@
-﻿using MongoDB.Bson;
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Conventions;
+using MongoDBMigrations;
+using Serilog;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
 
 namespace Watchman.Integrations.Database.MongoDB
 {
@@ -10,19 +15,42 @@ namespace Watchman.Integrations.Database.MongoDB
     {
         private static bool _initialized;
 
-        public static void Initialize()
+        public static void Initialize(IConfigurationRoot configuration)
         {
             if (_initialized)
             {
                 return;
             }
             RegisterConventions();
+            ApplyMigrations(configuration);
             _initialized = true;
         }
 
         private static void RegisterConventions()
         {
             ConventionRegistry.Register("DevscordConventions", new MongoConventions(), x => true);
+        }
+
+        private static void ApplyMigrations(IConfigurationRoot configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Mongo");
+            var databaseName = connectionString.Split('/').Last();
+            var assemblyForMigrations = Assembly.GetExecutingAssembly();
+            
+            var result = new MigrationEngine()
+                .UseDatabase(connectionString, databaseName)
+                .UseAssembly(assemblyForMigrations)
+                .UseSchemeValidation(enabled: false)
+                .UseProgressHandler(context => 
+                    Log.Information($@"The migration with name ""{context.MigrationName}"" from {context.TargetVersion} version was processed as {context.CurrentNumber} from all {context.TotalCount} migrations"))
+                .Run();
+
+            if (!result.Success)
+            {
+                // TODO: Do something when migrations throw exceptions.
+            }
+
+            Log.Information($"The current version of the database after data migrations is {result.CurrentVersion}");
         }
 
         private class MongoConventions : IConventionPack
