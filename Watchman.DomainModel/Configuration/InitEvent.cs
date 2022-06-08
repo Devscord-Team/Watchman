4211c3f703815f38dac254c240f9b155aafb9a9c
DavidPL-coder <dawid2002@hotmail.com>
2022-06-08 21:23:01 +0200

    Upgrade databse to 1.3.0 version as example of using the migrations system.

--- Watchman.DomainModel/Configuration/InitEvent.cs
+++ Watchman.DomainModel/Configuration/InitEvent.cs
@@ -7,11 +7,17 @@ namespace Watchman.DomainModel.Configuration
     {
         public ulong ServerId { get; private set; }
         public DateTime EndedAt { get; private set; }
+        public int EndedDay { get; private set; }
+        public int EndedHour { get; private set; }
+        public int EndedMinute { get; private set; }
 
         public InitEvent(ulong serverId, DateTime endedAt)
         {
             this.ServerId = serverId;
             this.EndedAt = endedAt;
+            this.EndedDay = endedAt.Day;
+            this.EndedHour = endedAt.Hour;
+            this.EndedMinute = endedAt.Minute;
         }
     }
 }
