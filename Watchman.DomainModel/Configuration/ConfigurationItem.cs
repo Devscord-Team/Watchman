6f82b4aeeac6051f8c1095d20e7e05a61ec210b3
DavidPL-coder <dawid2002@hotmail.com>
2022-06-09 22:25:01 +0200

    Remove example migration with 1.1.0 version and related things.

--- Watchman.DomainModel/Configuration/ConfigurationItem.cs
+++ Watchman.DomainModel/Configuration/ConfigurationItem.cs
@@ -12,7 +12,6 @@ namespace Watchman.DomainModel.Configuration
         public string Name { get; private set; }
         public string Group { get; private set; }
         public string SubGroup { get; private set; }
-        public string NameCopy { get; private set; }
 
         public ConfigurationItem(object value, ulong serverId, string name, string group, string subGroup)
         {
@@ -21,7 +20,6 @@ namespace Watchman.DomainModel.Configuration
             this.Name = name;
             this.Group = group;
             this.SubGroup = subGroup;
-            this.NameCopy = name;
         }
 
         public void SetValue(object value)
